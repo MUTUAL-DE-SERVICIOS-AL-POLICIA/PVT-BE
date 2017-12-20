0279ccb509afeb6f060c4994843abed34fba95c2
alucarth <ltorrezs2008@gmail.com>
2017-12-20 09:29:04 -0400

    mejoras en el inspinia

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -6,10 +6,10 @@
  */
 
 require('./bootstrap');
+require('./inspinia');
 
 window.Vue = require('vue');
 
-require('./inspinia');
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -18,6 +18,4 @@ require('./inspinia');
 
 Vue.component('example', require('./components/Example.vue'));
 
-const app = new Vue({
-    el: '#app'
-});
+
