f808a6c8f9f4d8ec3c2ce1c3887477594c7ff621
alucarth <ltorrezs2008@gmail.com>
2018-01-23 17:09:58 -0400

    borrando la imagen de su mujer de la lechuza XD

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -3,7 +3,7 @@
         <ul class="nav metismenu" id="side-menu">
             <li class="nav-header">
                 <div class="dropdown profile-element"> <span>
-                            <img alt="image" class="img-circle" src="images/lechuza.jpg" />
+                            <img alt="image" class="img-circle" src="images/user.png" />
                              </span>
                     <a data-toggle="dropdown" class="dropdown-toggle" href="#">
                             <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">{{Auth::user()->username}}</strong>
