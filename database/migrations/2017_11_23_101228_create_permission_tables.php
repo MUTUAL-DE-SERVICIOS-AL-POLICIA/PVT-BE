01a2af7b45407703e1d7605b27df077d5ecaa9dc
vrian7 <brian.barrera.bo@gmail.com>
2018-01-29 16:54:59 -0400

    migration, seeder, controllers

--- database/migrations/2017_11_23_101228_create_permission_tables.php
+++ database/migrations/2017_11_23_101228_create_permission_tables.php
@@ -13,18 +13,18 @@ class CreatePermissionTables extends Migration
     public function up()
     {
         $tableNames = config('permission.table_names');
-
+    if (!Schema::hasTable('model_has_permissions')) {
         Schema::create($tableNames['permissions'], function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('guard_name');
             $table->timestamps();
         });
-
-        Schema::table($tableNames['roles'], function (Blueprint $table) {          
-            $table->string('guard_name')->nullable();
-        });
-
+    }
+//        Schema::table($tableNames['roles'], function (Blueprint $table) {          
+//            $table->string('guard_name')->nullable();
+//        });
+    if (!Schema::hasTable('model_has_permissions')) {
         Schema::create($tableNames['model_has_permissions'], function (Blueprint $table) use ($tableNames) {
             $table->integer('permission_id')->unsigned();
             $table->morphs('model');
@@ -36,7 +36,9 @@ class CreatePermissionTables extends Migration
 
             $table->primary(['permission_id', 'model_id', 'model_type']);
         });
-
+    }
+    
+    if (!Schema::hasTable('model_has_roles')) {
         Schema::create($tableNames['model_has_roles'], function (Blueprint $table) use ($tableNames) {
             $table->integer('role_id')->unsigned();
             $table->morphs('model');
@@ -48,7 +50,9 @@ class CreatePermissionTables extends Migration
 
             $table->primary(['role_id', 'model_id', 'model_type']);
         });
-
+    }
+    
+    if (!Schema::hasTable('role_has_permissions')) {
         Schema::create($tableNames['role_has_permissions'], function (Blueprint $table) use ($tableNames) {
             $table->integer('permission_id')->unsigned();
             $table->integer('role_id')->unsigned();
@@ -68,6 +72,7 @@ class CreatePermissionTables extends Migration
             Artisan::call('cache:forget', ['key' => 'spatie.permission.cache']);
         });
     }
+    }
 
     /**
      * Reverse the migrations.
@@ -77,11 +82,13 @@ class CreatePermissionTables extends Migration
     public function down()
     {
         $tableNames = config('permission.table_names');
-
-        Schema::drop($tableNames['role_has_permissions']);
-        Schema::drop($tableNames['model_has_roles']);
-        Schema::drop($tableNames['model_has_permissions']);
-        Schema::drop($tableNames['roles']);
-        Schema::drop($tableNames['permissions']);
+//         Schema::table('roles', function (Blueprint $table) {
+//            $table->dropColumn('guard_name');
+//        });
+//        Schema::drop($tableNames['roles']);
+//        Schema::drop($tableNames['role_has_permissions']);
+//        Schema::drop($tableNames['model_has_roles']);
+//        Schema::drop($tableNames['model_has_permissions']);        
+//        Schema::drop($tableNames['permissions']);
     }
 }
