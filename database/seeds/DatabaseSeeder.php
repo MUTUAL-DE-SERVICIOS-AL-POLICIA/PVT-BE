e8f3cef96398201ccf28b3fb880b5a2f2bb5e230
alucarth <ltorrezs2008@gmail.com>
2018-02-02 10:17:30 -0400

    mensaje

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(ProcedureTablesSeeder::class);
+        $this->command->info('Todo ok :) finalizado!'); 
     }
 }
