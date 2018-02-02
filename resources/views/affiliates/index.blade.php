8febc21d980ca75bbd9519a51509e2f40a7093b6
Felix Balderrama <felixbvf@gmail.com>
2018-02-02 10:24:09 -0400

    Clean

--- resources/views/affiliates/index.blade.php
+++ resources/views/affiliates/index.blade.php
@@ -13,46 +13,12 @@
     <div class="row">
         <div class="col-lg-12">
             <div class="text-center m-t-lg">
-                <affiliate-index></affiliate-index>
-                {{-- <table id="affiliates-table" class="table table-bordered">
-                    <thead>
-                    <tr>
-                        <th>CI</th>
-                        <th>Primer Nombre</th>
-                        <th>Segundo Nombre</th>
-                        <th>Apellido Paterno</th>
-                        <th>Apellido Materno</th>
-                    </tr>
-                    </thead>
-                </table> --}}
-                <h2>
-                    Welcome in INSPINIA Larasssvel Starter Project
-                </h2>
-                <small>
-                    It is an application skeleton for a typical web app. You can use it to quickly bootstrap your webapp projects.
-                </small>
+                <affiliate-index></affiliate-index>         
             </div>
         </div>
     </div>
 </div>
 @endsection
 @section('scripts')
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script>
-    $(document).ready(function() {
-        $('#affiliates-table').DataTable({
-            serverSide: true,
-            processing: true,
-            ajax: '/get_all_affiliates',
-            columns: [
-                {data: 'id'},
-                {data: 'first_name'},
-                {data: 'second_name'},
-                {data: 'last_name'},
-                {data: 'mothers_last_name'},
-                // {data: 'action', orderable: false, searchable: false}
-            ]
-        });
-    });
-</script>
+
 @endsection
