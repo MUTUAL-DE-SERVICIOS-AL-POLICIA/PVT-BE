9fbaa0ac5d418b9b70d0ffd7668a4179d671ae55
Ale Guis F <aleguis@icloud.com>
2018-02-05 15:13:01 -0400

    Update 2017_08_31_091916_create_retirement_fund_tables.php

--- database/migrations/2017_08_31_091916_create_retirement_fund_tables.php
+++ database/migrations/2017_08_31_091916_create_retirement_fund_tables.php
@@ -62,6 +62,9 @@ class CreateRetirementFundTables extends Migration {
             $table->bigInteger('ret_fun_procedure_id')->unsigned()->nullable(); //identificador de tipo de modalidad
             $table->bigInteger('city_start_id')->unsigned()->nullable(); //ciudad donde se inicia el tramite.
             $table->bigInteger('city_end_id')->unsigned()->nullable(); //ciudad donde se entrega el pago.
+            $table->bigInteger('workflow_id')->unsigned(); // identificador de flujo
+            $table->bigInteger('wf_state_current_id')->unsigned(); //identificador de flujo de estado
+            
             $table->string('code')->unique(); //codigo
             $table->date('reception_date')->nullable(); //fecha de recepcion
             $table->enum('type', ['Pago', 'Anticipo'])->default('Pago'); //tipo
@@ -73,6 +76,8 @@ class CreateRetirementFundTables extends Migration {
             $table->foreign('ret_fun_procedure_id')->references('id')->on('ret_fun_procedures');
             $table->foreign('city_start_id')->references('id')->on('cities');
             $table->foreign('city_end_id')->references('id')->on('cities');
+            $table->foreign('workflow_id')->references('id')->on('workflows');
+            $table->foreign('wf_state_current_id')->references('id')->on('wf_states');
             $table->timestamps();
             $table->softDeletes();
         });
