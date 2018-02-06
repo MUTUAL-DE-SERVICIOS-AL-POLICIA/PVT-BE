6473edab2589406cc4dbde439376efc73273bc13
miv1 <m.iriarte.val@gmail.com>
2018-02-06 11:10:01 -0400

    Agregar modulos RetFunProcedures, RetirementFunds

--- database/seeds/add_kinship.php
+++ database/seeds/add_kinship.php
@@ -19,13 +19,6 @@ class add_kinship extends Seeder
         ['id' => '5', 'name' => 'Apoderado'],
         ['id' => '6', 'name' => 'Tutor/Tutora'],
         ['id' => '7', ]
-        ]);
-
-       /* DB::table('procedure_types')->insert([
-            ['module_id' => '3', 'name' => 'Pago Global de Aportes'],
-            ['module_id' => '3', 'name' => 'Pago de Fondo de Retiro'],
-            ['module_id' => '4', 'name' => 'Pago Cuota Mortuoria'],
-            ['module_id' => '5', 'name' => 'Pago Auxilio Mortuorio'],            
-        ]);*/
+        ]);       
     }
 }
