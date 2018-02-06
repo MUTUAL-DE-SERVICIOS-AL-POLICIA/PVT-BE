78e4bda552b06084af59d59ad58d2084f99c6a7f
miv1 <m.iriarte.val@gmail.com>
2018-02-06 09:05:21 -0400

    Eliminacion de las columnas address de beneficiario Q-A

--- database/migrations/2017_11_23_101228_create_permission_tables.php
+++ database/migrations/2017_11_23_101228_create_permission_tables.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
@@ -82,13 +82,13 @@ class CreatePermissionTables extends Migration
     public function down()
     {
         $tableNames = config('permission.table_names');
-//         Schema::table('roles', function (Blueprint $table) {
-//            $table->dropColumn('guard_name');
-//        });
-//        Schema::drop($tableNames['roles']);
-//        Schema::drop($tableNames['role_has_permissions']);
-//        Schema::drop($tableNames['model_has_roles']);
-//        Schema::drop($tableNames['model_has_permissions']);        
-//        Schema::drop($tableNames['permissions']);
+         Schema::table('roles', function (Blueprint $table) {
+            $table->dropColumn('guard_name');
+        });
+        Schema::drop($tableNames['roles']);
+        Schema::drop($tableNames['role_has_permissions']);
+        Schema::drop($tableNames['model_has_roles']);
+        Schema::drop($tableNames['model_has_permissions']);        
+        Schema::drop($tableNames['permissions']);
     }
 }
