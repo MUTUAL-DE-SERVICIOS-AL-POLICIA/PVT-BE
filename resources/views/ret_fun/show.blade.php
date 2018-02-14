a042c0d358493b0effb949b8cbabe7d3ec557166
cechus <cechuscsc4@gmail.com>
2018-02-14 09:16:18 -0400

    bug fixes navbar, and added PrintJS

--- resources/views/ret_fun/show.blade.php
+++ resources/views/ret_fun/show.blade.php
@@ -11,20 +11,7 @@
 </div>
 <div class="wrapper wrapper-content animated fadeInRight">
     <div class="row text-center">
-        <button class="btn btn-primary dim" type="button" data-toggle="tooltip" data-placement="top" title="Imprimir recepción"><i class="fa fa-print"></i></button>
-        {{--  <button class="btn btn-warning dim" type="button"><i class="fa fa-warning"></i></button>
-        <button class="btn btn-primary dim" type="button"><i class="fa fa-check"></i></button>
-        <button class="btn btn-success  dim" type="button"><i class="fa fa-upload"></i></button>
-        <button class="btn btn-info  dim" type="button"><i class="fa fa-paste"></i> </button>
-        <button class="btn btn-warning  dim" type="button"><i class="fa fa-warning"></i></button>
-        <button class="btn btn-default  dim " type="button"><i class="fa fa-star"></i></button>
-        <button class="btn btn-danger  dim " type="button"><i class="fa fa-heart"></i></button>--}}
-    </div>
-    <div class="row">
-        <div class="col-lg-12">
-            <input type="button" class="btn btn-info" value="Generar PDF"  target="_blank" onclick="location.href = '{{asset("ret_fun/".$retirement_fund->id."/print/reception")}}';">
-        </div>
-
+        <button class="btn btn-primary dim" type="button" data-toggle="tooltip" data-placement="top" title="Imprimir recepción" onclick="printJS({printable:'{!! route('ret_fun_print_reception', $retirement_fund->id) !!}', type:'pdf', showModal:true})"><i class="fa fa-print"></i></button>
     </div>
     <div class="row">
         <div class="col-md-6">
@@ -41,7 +28,7 @@
             @include('ret_fun.beneficiaries_list', ['beneficiaries'=>$beneficiaries])
         </div>
     </div>
-    
+
     <div class="row">
         <div class="col-md-6">
             @include('affiliates.folder', ['folders'=>$affiliate->affiliate_folders,'procedure_modalities'=>$procedure_modalities,'affiliate_id'=>$affiliate->id])
