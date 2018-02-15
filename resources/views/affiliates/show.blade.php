74d560970537f5fc5dbc42d6c779ff0a5061f295
alucarth <ltorrezs2008@gmail.com>
2018-02-15 18:51:53 -0400

    cuasi terminado

--- resources/views/affiliates/show.blade.php
+++ resources/views/affiliates/show.blade.php
@@ -45,7 +45,7 @@
                                         <dt>Genero:</dt> <dd>{{ $affiliate->gender }}</dd>
                                         <dt>Estado Civil:</dt> <dd>{{ $affiliate->civil_status }}</dd>
                                         <dt>Fecha de Nacimiento:</dt> <dd>{{ $affiliate->birth_date }}</dd>
-                                        <dt>Edad:</dt> <dd>{{ $affiliate->birth_date }}</dd>
+                                        <dt>Edad:</dt> <dd> @{{ age }} </dd>
                                         <dt>Lugar de Nacimiento:</dt> <dd>{{ !!$affiliate->city_birth ? $affiliate->city_birth->name : '' }}</dd>
                                         <dt>Telefono:</dt> <dd>{{ $affiliate->phone_number }}</dd>
                                         <dt>Celular:</dt> <dd>{{ $affiliate->cell_phone_number }}</dd>
@@ -81,7 +81,7 @@
                                         <dt>Primer Nombre:</dt> <dd><input type="text" v-model="form.first_name" class="form-control"></dd>
                                         <dt>Segundo Nombre:</dt> <dd><input type="text" v-model="form.second_name" class="form-control"></dd>
                                         <dt>Apellido Paterno:</dt> <dd><input type="text" v-model="form.last_name" class="form-control"></dd>
-                                        <dt>Apellido Materno:</dt> <dd><input type="text" class="form-control"></dd>
+                                        <dt>Apellido Materno:</dt> <dd><input type="text" v-model="form.mothers_last_name" class="form-control"></dd>
                                         <dt v-show="affiliate.gender === 'F'">Apellido de Casada:</dt> <dd v-show="affiliate.gender === 'F'"><input type="text" class="form-control"></dd>
                                     </dl>
                                 </div>
@@ -92,13 +92,13 @@
                                         
                                         <dt>Fecha de Nacimiento:</dt> <dd>
                                             <div class="input-group date" >
-                                            <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input  v-model="form.birth_date" type="text" class="form-control" value="03/04/2014">
+                                            <span class="input-group-addon"><i class="fa fa-calendar"></i></span><input  v-model="form.birth_date" type="text" class="form-control">
                                             </div>
                                             </dd>
-                                        <dt>Edad:</dt> <dd><input type="text" class="form-control"></dd>
-                                        <dt>Lugar de Nacimiento:</dt> <dd><input type="text" class="form-control"></dd>
+                                        <dt>Edad:</dt> <dd><input v-model="age" type="text" class="form-control" disabled></dd>
+                                        <dt>Lugar de Nacimiento:</dt> <dd><input v-model="form.birth_date" type="text" class="form-control"></dd>
                                         <dt>Telefono:</dt> <dd><input type="text" v-model="form.phone_number" class="form-control"></dd>
-                                        <dt>Celular:</dt> <dd><input type="text" class="form-control"></dd>
+                                        <dt>Celular:</dt> <dd><input type="text" v-model="form.cell_phone_number" class="form-control"></dd>
                                     </dl>
                                 </div>
                             </div>
