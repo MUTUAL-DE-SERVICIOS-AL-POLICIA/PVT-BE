85cb0ae8cd12932034a8688f19cbbc93c089d63e
Ale Guis F <aleguis@icloud.com>
2018-02-20 18:22:51 -0400

    bug fixes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(add_kinship::class);
+        $this->call(add_procedure_types::class);
         $this->call(ProcedureTablesSeeder::class);
         $this->call(QuotaSeeder::class);
         $this->call(WorkFlowAndStates::class);
