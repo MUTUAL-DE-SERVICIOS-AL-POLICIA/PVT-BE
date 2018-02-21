1189264dea5d1068d6fb26acb38a87d70eb6d82f
miv1 <m.iriarte.val@gmail.com>
2018-02-21 15:51:20 -0400

    Modification Contribution

--- database/seeds/WorkFlowAndStates.php
+++ database/seeds/WorkFlowAndStates.php
@@ -11,13 +11,12 @@ class WorkFlowAndStates extends Seeder
      */
     public function run()
     {
-        //
-        DB::table('wf_states')->insert([
-        	['id'=>19, 'module_id'=>3,'role_id'=>10,'name'=> 'Área de Recepción Fondo de Retiro','first_shortened'=>'Recepción'],
-        	['id'=>20, 'module_id'=>3,'role_id'=>15,'name'=> 'Área de Archivo Fondo de Retiro','first_shortened'=>'Archivo'],
-        	['id'=>21, 'module_id'=>3,'role_id'=>14,'name'=> 'Área de Legal Fondo de Retiro','first_shortened'=>'Legal'],
-        ]);
-
+        
+        DB::table('procedure_modalities')->insert([
+            ['id' => '13','procedure_type_id' => '3', 'name' => 'Anticipo Fondo de Retiro Policial', 'is_valid'=> false],
+            ['id' => '14','procedure_type_id' => '3', 'name' => 'Aporte Voluntario Item "0" ', 'is_valid' => true],
+            ['id' => '15','procedure_type_id' => '3', 'name' => 'Expediente Transitorio ', 'is_valid' =>true],
+            ]);  
         // DB::table('wf_sequences')->insert([
         // 	['workflow_id'=>4,'wf_state_current_id' =>19 ,'wf_state_next_id'=>20,'action'=>'Aprobar' ,'created_at'=>'2018/02/02','updated_at'=>'2018/02/02' ],
         // 	['workflow_id'=>4,'wf_state_current_id' =>20 ,'wf_state_next_id'=>21, 'action'=> 'Aprobar' ,'created_at'=>'2018/02/02','updated_at'=>'2018/02/02' ],
--- database/seeds/add_fake_registers.php
+++ database/seeds/add_fake_registers.php
@@ -14,7 +14,7 @@ class add_fake_registers extends Seeder
     {
         $faker = F::create('App\Users');
                 
-        for($i=1;$i<150;$i++)
+        for($i=1;$i<30;$i++)
         {
             DB::table('retirement_funds')->insert([
                 'user_id'=>$faker->numberBetween($min = 1, $max = 20),
@@ -23,7 +23,7 @@ class add_fake_registers extends Seeder
                 'ret_fun_procedure_id' => 1,
                 'city_start_id' => $faker -> numberBetween($min = 1, $max = 9),
                 'city_end_id' => $faker -> numberBetween($min = 1, $max = 9),
-                'code' => str_random(8),
+                'code' => $faker->numerify('###/2018'),                
                 'reception_date' => $faker -> date($format = 'Y-m-d', $max = 'now'),     
                 'type' => $faker -> randomElement($array = array ('Pago','Anticipo')),
                 'subtotal' =>$faker-> numberBetween($min = 1000, $max = 10000), 
@@ -36,7 +36,7 @@ class add_fake_registers extends Seeder
         for($i=1;$i<3;$i++)
         {
             DB::table('quota_aid_procedures')->insert([
-                'hierarchy_id'=>$faker->numberBetween($min = 1, $max = 6),
+                'hierarchy_id'=>$faker->numberBetween($min = 1, $max = 5),
                 'type_mortuary' => $faker -> randomElement($array = array ('Titular','Conyuge', 'Viuda')),
                 'procedure_modality_id' => $faker->numberBetween($min = 1, $max = 12),                
                 'amount' => $faker -> numberBetween($min = 1, $max = 12),
@@ -50,7 +50,7 @@ class add_fake_registers extends Seeder
                 'user_id'=>$faker->numberBetween($min = 1, $max = 20),
                 'affiliate_id' => $faker -> numberBetween($min = 60, $max = 190),
                 'quota_aid_procedure_id' => $faker->numberBetween($min = 1, $max = 2),                
-                'procedure_modalities_id' => $faker -> numberBetween($min = 1, $max = 12),
+                'procedure_modality_id' => $faker -> numberBetween($min = 1, $max = 12),
                'city_start_id' => $faker -> numberBetween($min = 1, $max = 9),
                 'city_end_id' => $faker -> numberBetween($min = 1, $max = 9),
                 'code' => str_random(8),
