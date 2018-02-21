8469f46d53345764ce62ad373fd5014adb66f87d
cechus <cechuscsc4@gmail.com>
2018-02-21 14:36:49 -0400

    bug fixes certifications, added search button on edit beneficiarires

--- resources/assets/js/components/ret_fun/Beneficiary.vue
+++ resources/assets/js/components/ret_fun/Beneficiary.vue
@@ -42,7 +42,7 @@
             <div class="col-md-4">
                 <div class="form-group"><label class="col-sm-4 control-label">Ciudad de Expedicion</label>
                     <div class="col-sm-8">
-                        <select class="form-control m-b" v-model="beneficiary.city_identity_card" name="beneficiary_city_identity_card[]">
+                        <select class="form-control m-b" v-model="beneficiary.city_identity_card_id" name="beneficiary_city_identity_card[]">
                             <option v-for="city in cities" :key="city.id" :value="city.id" >{{ city.name }}</option>
                         </select>
                     </div>
@@ -58,7 +58,7 @@
             <div class="col-md-4">
                 <div class="form-group"><label class="col-sm-4 control-label">Parentesco</label>
                     <div class="col-sm-8">
-                        <select class="form-control m-b" v-model="beneficiary.kinship" name="beneficiary_kinship[]">
+                        <select class="form-control m-b" v-model="beneficiary.kinship_id" name="beneficiary_kinship[]">
                             <option v-for="kinship in kinships" :key="beneficiary.id + ''+kinship.id " :value="kinship.id">{{kinship.name}}</option>
                         </select>
                     </div>
@@ -68,7 +68,6 @@
         <div class="hr-line-dashed"></div>
     </div>
     <div v-else>
-        
         <div class="row" >
             <div class="row"> <button class="btn btn-danger" type="button" v-on:click= "remove"> <i class="fa fa-times"></i> </button></div>
             <div class="col-md-6">
@@ -110,7 +109,7 @@
             <div class="col-md-4">
                 <div class="form-group"><label class="col-sm-4 control-label">Ciudad de Expedicion</label>
                     <div class="col-sm-8">
-                        <select class="form-control m-b" v-model="beneficiary.city_identity_card" disabled >
+                        <select class="form-control m-b" v-model="beneficiary.city_identity_card_id" disabled >
                             <option v-for="city in cities" :key="city.id" :value="city.id" >{{ city.name }}</option>
                         </select>
                     </div>
@@ -126,7 +125,7 @@
             <div class="col-md-4">
                 <div class="form-group"><label class="col-sm-4 control-label">Parentesco</label>
                     <div class="col-sm-8">
-                        <select class="form-control m-b" v-model="beneficiary.kinship" disabled >
+                        <select class="form-control m-b" v-model="beneficiary.kinship_id" disabled >
                             <option v-for="kinship in kinships" :key="beneficiary.id + ''+kinship.id " :value="kinship.id">{{kinship.name}}</option>
                         </select>
                     </div>
@@ -174,9 +173,9 @@ export default {
       this.beneficiary.mothers_last_name = data.mothers_last_name;
       this.beneficiary.surname_husband = data.surname_husband;
       this.beneficiary.identity_card = data.identity_card;
-      this.beneficiary.city_identity_card = data.city_identity_card_id;
+      this.beneficiary.city_identity_card_id = data.city_identity_card_id;
       this.beneficiary.birth_date = data.birth_date;
-      this.beneficiary.kinship = data.kinship_id;
+      this.beneficiary.kinship_id = data.kinship_id;
     }
   }
 };
