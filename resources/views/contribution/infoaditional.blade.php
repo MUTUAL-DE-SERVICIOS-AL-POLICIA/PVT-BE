2a9309aac36dc72ce23d98b9b54c084f974ebe97
Norah995 <carmesi995@gmail.com>
2018-02-22 11:36:25 -0400

    infoaditional

---
+++ resources/views/contribution/infoaditional.blade.php
@@ -0,0 +1,83 @@
+<div class="col-md-6">
+    <div class="box box-success box-solid">
+        <div class="box-header with-border">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3 class="box-title"><span class="glyphicon glyphicon-info-sign"></span> Información Adicional</h3>
+                </div>
+            </div>
+        </div>
+        <div class="box-body">
+            <div class="row">
+                <div class="col-md-6">
+                    <table class="table table-responsive" style="width:100%;">
+                        <tr>
+                            <td>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Fecha de Ingreso
+                                    </div>
+                                    <div class="col-md-6">
+                                        aaa
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Total Aporte
+                                    </div>
+                                    <div class="col-md-6">
+                                        Bs 
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Saldo Inicial
+                                    </div>
+                                    <div class="col-md-6">
+                                         
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+                <div class="col-md-6">
+                    <table class="table" style="width:100%;">
+                        <tr>
+                            <td>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Total Fondo Retiro
+                                    </div>
+                                    <div class="col-md-6">
+                                        Bs 
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        Total Cuota Mortuoria
+                                    </div>
+                                    <div class="col-md-6">
+                                        Bs 
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
