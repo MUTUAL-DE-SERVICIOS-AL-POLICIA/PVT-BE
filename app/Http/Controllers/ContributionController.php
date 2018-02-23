8ef4b285115f058f0b8d571bcee6006c1abc4d78
Felix Balderrama <felixbvf@gmail.com>
2018-02-23 11:29:57 -0400

    bug fixes

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -5,11 +5,7 @@ namespace Muserpol\Http\Controllers;
 use Muserpol\Models\Contribution;
 use Illuminate\Http\Request;
 use Muserpol\Models\Affiliate;
-use Muserpol\Models\AffiliateState;
-use Muserpol\Models\Category;
-use Muserpol\Models\City;
-use Muserpol\Models\Degree;
-use Muserpol\Models\PensionEntity;
+
 class ContributionController extends Controller
 {
     /**
@@ -89,31 +85,11 @@ class ContributionController extends Controller
         //
     }
     public function generateContribution(Affiliate $affiliate)
-    {   $cities = City::all()->pluck('first_shortened', 'id');
-        $birth_cities = City::all()->pluck('name', 'id');
-        $categories = Category::all()->pluck('name', 'id');
-        $degrees = Degree::all()->pluck('name', 'id');
-        $pension_entities = PensionEntity::all()->pluck('name', 'id');
-        $affiliate_states = AffiliateState::all()->pluck('name', 'id');
-
-        $affiliate->load([
-            'city_identity_card:id,first_shortened',
-            'city_birth:id,name',
-            'affiliate_state',
-            'pension_entity',
-            'category',
-            'degree',
-        ]);
+    {   
         
         $data = array(
            
-            'affiliate'=>$affiliate,
-            'cities'=>$cities,
-            'birth_cities'=>$birth_cities,
-            'categories'=>$categories,
-            'degrees'=>$degrees,
-            'pension_entities' =>$pension_entities,
-            'affiliate_states'=>$affiliate_states
+            'affiliate'=>$affiliate,          
            
         );
         return View('contribution.create')->with($data);
