10ba13a16e78178093a85d8db753e6586fe88700
miv1 <m.iriarte.val@gmail.com>
2018-04-03 19:00:53 -0400

    solving bugs direct_aid_contribution

--- app/Http/Controllers/AidContributionController.php
+++ app/Http/Controllers/AidContributionController.php
@@ -199,16 +199,6 @@ class AidContributionController extends Controller
         $cities = City::all()->pluck('first_shortened', 'id');
         $birth_cities = City::all()->pluck('name', 'id');
         //get Commitment data
-<<<<<<< HEAD
-//        $commitment = ContributionCommitment::where('affiliate_id',$affiliate->id)->where('state','ALTA')->first();        
-//        if(!isset($commitment->id))
-//        {
-//            $commitment = new ContributionCommitment();
-//            $commitment->id = 0;
-//            $commitment->affiliate_id = $affiliate->id;
-//        }
-
-=======
        $aid_commitment = AidCommitment::where('affiliate_id',$affiliate->id)->first();     
        if(!isset($aid_commitment->id))
        {
@@ -216,7 +206,6 @@ class AidContributionController extends Controller
            $aid_commitment->id = 0;
            $aid_commitment->affiliate_id = $affiliate->id;
        }
->>>>>>> upstream/master
         $data = [
             'contributions' => $group,            
             'affiliate_id' => $affiliate->id,            
@@ -226,17 +215,10 @@ class AidContributionController extends Controller
             'affiliate' => $affiliate,
             'cities' => $cities,
             'birth_cities' => $birth_cities,
-<<<<<<< HEAD
-            'commitment'    =>  $commitment,
-            'today_date' =>  date('Y-m-d'),
-            ];
-        
-=======
             //'new_contributions' => self::getMonthContributions($affiliate->id),            
             'aid_commitment'    =>  $aid_commitment,
             'today_date'         =>  date('Y-m-d'),
         ];
->>>>>>> upstream/master
         //return  date('Y-m-d');
          return view('contribution.affiliate_aid_contributions_edit', $data);
     }
