f078f90170878e59d6a54405b4b081a225582811
alucarth <ltorrezs2008@gmail.com>
2018-04-05 10:33:37 -0400

    breadcumb hdp

--- resources/views/contribution/select.blade.php
+++ resources/views/contribution/select.blade.php
@@ -3,15 +3,14 @@
 @section('title', 'Seleccion de Aportes')
 
 @section('content')
-
 <div class="row wrapper border-bottom white-bg page-heading">
     <div class="col-lg-9">
-        
+        {!!Breadcrumbs::render('classify_contributions',$ret_fun)!!}
     </div>
 </div>
 
 <div class="wrapper wrapper-content animated fadeInRight">
     {{--  <contribution-create :contributions1="{{ json_encode($contributions) }}" :afid="{{ $affiliate->id }}" ></contribution-create>  --}}
-<contribution-select :contributions ="{{json_encode($contributions)}}" :retfunid="{{$ret_fun->id}}" :contype="{{json_encode($con_type)}}" :types="{{json_encode($contribution_types)}}"></contribution-select>
+    <contribution-select :contributions ="{{json_encode($contributions)}}" :retfunid="{{$ret_fun->id}}" :contype="{{json_encode($con_type)}}" :types="{{json_encode($contribution_types)}}"></contribution-select>
 </div>
 @endsection
