f14122c2a2ab37c29c00f66f51b0778a0c2b75ff
Alucarth <ltorrezs2008@gmail.com>
2018-04-27 18:59:59 -0400

    historial migracion

---
+++ database/migrations/2018_04_27_115611_records.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Records extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('affiliate_records_pvt', function (Blueprint $table) {// historial del afiliado como ya existe la tabla affilate_record le puse pvt(plataforma virtual de tramites hdps)
+                $table->bigIncrements('id');
+                $table->bigInteger('user_id')->unsigned();
+                $table->bigInteger('affiliate_id')->unsigned();
+                $table->bigInteger('message')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('affiliate_id')->references('id')->on('affiliates');
+                $table->timestamps();
+        });
+        Schema::create('ret_fun_records', function (Blueprint $table) {// historial para los tramites de fondo de retiro
+                $table->bigIncrements('id');
+                $table->bigInteger('user_id')->unsigned();
+                $table->bigInteger('ret_fun_id')->unsigned();
+                $table->bigInteger('message')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('ret_fun_id')->references('id')->on('retirement_funds');
+                $table->timestamps();
+        });
+        Schema::create('sequences_records', function (Blueprint $table) {// historial para las secuencias
+                $table->bigIncrements('id');
+                $table->bigInteger('user_id')->unsigned();
+                $table->bigInteger('wf_state_id')->unsigned();
+                $table->bigInteger('message')->unsigned();
+                $table->foreign('user_id')->references('id')->on('users');
+                $table->foreign('wf_states_id')->references('id')->on('wf_states');
+                $table->timestamps();
+        });
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
