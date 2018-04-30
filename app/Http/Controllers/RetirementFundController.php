fae3119ade443f55bf1dedde285d7e9747ea9b15
vrian7 <i-am-brian@hotmail.com>
2018-04-30 10:27:57 -0400

    arreglo de listado de fondos de retiro

--- app/Http/Controllers/RetirementFundController.php
+++ app/Http/Controllers/RetirementFundController.php
@@ -443,7 +443,7 @@ class RetirementFundController extends Controller
         
                                 
         $ret_funds = RetirementFund::select('retirement_funds.id','affiliates.first_name as first_name','affiliates.last_name as last_name','procedure_modalities.name as modality','workflows.name as workflow','retirement_funds.code','retirement_funds.reception_date','retirement_funds.total')
-                                ->leftJoin('affiliates','retirement_funds.id','=','affiliates.id')
+                                ->leftJoin('affiliates','retirement_funds.affiliate_id','=','affiliates.id')
                                 ->leftJoin('procedure_modalities','retirement_funds.procedure_modality_id','=','procedure_modalities.id')
                                 ->leftJoin('workflows','retirement_funds.workflow_id','=','workflows.id')                               
                                 ->where('affiliates.first_name','LIKE',$first_name.'%')
