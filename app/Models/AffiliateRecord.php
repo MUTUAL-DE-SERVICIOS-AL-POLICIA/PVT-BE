e9e39f1d6d978e95cb51968c3aba5b8e9a89f500
Alucarth <ltorrezs2008@gmail.com>
2018-05-02 11:02:29 -0400

    historial del afiliado solo en base

---
+++ app/Models/AffiliateRecord.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Muserpol\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliateRecord extends Model
+{
+    //
+    protected $table = 'affiliate_records_pvt';
+    
+}
