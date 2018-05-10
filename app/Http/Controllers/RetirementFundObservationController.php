033041e80d6469acbc62257ce154b6f0ddef7540
Alucarth <ltorrezs2008@gmail.com>
2018-05-10 18:06:15 -0400

    registro de observaciones acabadas

---
+++ app/Http/Controllers/RetirementFundObservationController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace Muserpol\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Muserpol\Models\RetirementFund\RetFunObservation;
+use Auth;
+use Carbon\Carbon;
+
+class RetirementFundObservationController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $observation = new RetFunObservation;
+        $observation->user_id = Auth::user()->id;
+        $observation->observation_type_id = $request->observation_type_id;
+        $observation->retirement_fund_id = $request->retirement_fund_id;
+        $observation->is_enabled = $request->is_enabled;
+        $observation->message = $request->message;
+        $observation->date= Carbon::now()->format('Y/d/m');
+        $observation->save();
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
