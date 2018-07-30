ad14b5800ba530490d669efba7524159df1d63b5
ivangrover12 <ivang.monzon12@gmail.com>
2018-07-30 06:45:21 -0700

    En afiliados los cambios se modifica (automatico en mayuscula) al editarlo.#417

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -267,11 +267,7 @@ class AffiliateController extends Controller
             }
         }
 
-        $affiliate->identity_card = strtoupper($affiliate->identity_card);
-        $affiliate->first_name = strtoupper($affiliate->first_name);
-        $affiliate->second_name = strtoupper($affiliate->second_name);
-        $affiliate->last_name = strtoupper($affiliate->last_name);
-        $affiliate->mothers_last_name = strtoupper($affiliate->mothers_last_name);
+
 
         $affiliate->save();
         $affiliate = Affiliate::with('address')->find($affiliate->id);
