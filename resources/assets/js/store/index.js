bac1d98411d0c738203cd2648bb0332a05427fd1
cechus <cechuscsc4@gmail.com>
2018-08-19 00:41:28 -0400

    enhancements vuex

--- resources/assets/js/store/index.js
+++ resources/assets/js/store/index.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
  Vue.use(Vuex); 
 
-import retFunForm from './modules/retfun/form'
+import retFunForm from './modules/retFun/form'
 import quotaAidForm from './modules/quotaAid/form'
 import inbox from './modules/inbox'
 
 export default new Vuex.Store({
     modules: {
-      retfunform: retFunForm,
+      retFunForm: retFunForm,
       inbox: inbox,
       quotaAidForm, //quotaAidForm: quotaAidForm
     }
--- resources/assets/js/store/modules/inbox/index.js
+++ resources/assets/js/store/modules/inbox/index.js
@@ -39,4 +39,5 @@ export default {
     state,
     mutations,
     getters,
+    namespaced: true,
 }
