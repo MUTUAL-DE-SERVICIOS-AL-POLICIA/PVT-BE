baf221a628e04f520c9027016c3d9350fd926001
ivangrover12 <ivang.monzon12@gmail.com>
2018-08-22 10:11:01 -0400

    helper insert Ids

--- app/Helpers/Ids.php
+++ app/Helpers/Ids.php
@@ -4,14 +4,23 @@ namespace Muserpol\Helpers;
 
 class Ids
 {
+	//*@retorna el id del beneficiario
+	//*@no recibe ningun valor
+	//*@retorna un numero entero
 	public static function getBeneficiarioId()
 	{
 		return 1;
 	}
+	//*@retorna el id del beneficiario
+	//*@no recibe ningun valor
+	//*@retorna un numero entero
 	public static function getTutorId()
 	{
 		return 2;
 	}
+	//*retorna el id del beneficiario
+	//*no recibe ningun valor
+	//*retorna un numero entero
 	public static function getApoderadoId()
 	{
 		return 3;
