77de031f675e04795e98e4f9e77c165f9b400d4c
ivangrover12 <ivang.monzon12@gmail.com>
2018-08-22 10:12:49 -0400

    helper insert Ids

--- app/Helpers/Ids.php
+++ app/Helpers/Ids.php
@@ -11,14 +11,14 @@ class Ids
 	{
 		return 1;
 	}
-	//*@retorna el id del beneficiario
+	//*@retorna el id del Tutor
 	//*@no recibe ningun valor
 	//*@retorna un numero entero
 	public static function getTutorId()
 	{
 		return 2;
 	}
-	//*retorna el id del beneficiario
+	//*retorna el id del Apoderado
 	//*no recibe ningun valor
 	//*retorna un numero entero
 	public static function getApoderadoId()
