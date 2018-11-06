368e79602ce2670def23818336a2fb7bd7ca06c6
feliking <felixddxd@gmail.com>
2018-11-06 11:25:40 -0400

    Se añadió ordenamiento dinamico

--- config/app.php
+++ config/app.php
@@ -230,9 +230,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-	'Curl' => Ixudra\Curl\Facades\Curl::class,
-
+	    'Curl' => Ixudra\Curl\Facades\Curl::class,
         'Util' => Muserpol\Helpers\Util::class,
+        'Ldap' => App\Helpers\Ldap::class,
         //dependences
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
--- resources/views/auth/change.blade.php
+++ resources/views/auth/change.blade.php
@@ -7,28 +7,49 @@
     <h1 class="text-navy text-center">Plataforma Virtual de Trámites</h1>
 </div>
 <div class="wrapper wrapper-content animated fadeInRight">
-    <div class="row ng-scope text-center">
-
-
+    <div class="row text-center">
+        @php
+            $count = count($roles);
+            $style = "";  
+        @endphp
+        @if($count == 1)
+            @php
+                $style = "<div class='col-md-3 col-md-offset-4'>";
+            @endphp
+        @elseif($count == 2)
+            @php
+                $style = "<div class='col-md-3 col-md-offset-2'>";
+            @endphp
+        @elseif($count == 3)
+            @php
+                $style = "<div class='col-md-3 col-md-offset-1'>";
+            @endphp
+        @else
+            @php
+                $style = "<div class='col-md-3'>";
+            @endphp
+        @endif
         @foreach($roles as $role)
         {!! Form::open(['url' => 'postchangerol', 'role' => 'form']) !!}
-        
+                @php
+                    echo $style;    
+                @endphp
                 @if($role->module_id==1)
-                <button class="widget red-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget red-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-red">
                     <i class="glyphicon glyphicon-hdd fa-4x"></i><br>
                     <b>{{$role->name}}</b>
                 </span>
                 @endif
                 @if($role->module_id==2 || $role->module_id==8 )
-                <button class="widget lazur-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget lazur-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-aqua">
                     <i class="fa fa-puzzle-piece fa-5x"></i><br>
                     <b>{{$role->name}}</b>
                 </span>
                 @endif     
                 @if($role->module_id==3)
-                <button class="widget yellow-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget yellow-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-yellow">
                     <i class="glyphicon glyphicon-piggy-bank fa-4x"></i><br>
                     <b>{{$role->name}}</b>
@@ -37,7 +58,7 @@
                 @endif
 
                 @if($role->module_id==6 || $role->module_id==9)
-                <button class="widget blue-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget blue-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-green">
                     <i class="fa fa-money fa-5x"></i><br>
                     <b>{{$role->name}}</b>
@@ -45,7 +66,7 @@
                 
                 @endif
                 @if($role->module_id==4 ||$role->module_id==5 )
-                <button class="widget blue-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget blue-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-green">
                     <i class="fa fa-heartbeat fa-5x"></i><br>
                     <b>{{$role->name}}</b>
@@ -54,7 +75,7 @@
                 @endif
 
                 @if($role->module_id==7)
-                <button class="widget blue-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget blue-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-green">
                     <i class="fa fa-balance-scale fa-5x"></i><br>
                     <b>{{$role->name}}</b>
@@ -62,7 +83,7 @@
                 
                 @endif
                 @if($role->module_id==10)
-                <button class="widget green-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget green-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-green">
                     <i class="fa  fa-map fa-5x"></i><br>
                     <b>{{$role->name}}</b>
@@ -70,7 +91,7 @@
                 
                 @endif
                 @if($role->module_id == 11)
-                <button class="widget green-bg px-1 text-center col-lg-3" style="height: 200px">
+                <button class="widget green-bg px-1 text-center" style="height: 200px; width: 100%">
                 <span class="info-box-icon bg-green">
                     <i class="fa  fa-map fa-5x"></i><br>
                     <b>{{$role->name}}</b>
@@ -83,32 +104,10 @@
                 <span class="info-box-text"> {!! $role->module->name !!} </span>
                 </b>
                 <br>
-
-                @if($role->module_id==1)   
-                <input type="submit" class="btn btn-block btn-raised btn-danger"><i class="glyphicon glyphicon-share-alt"></i>
-                @endif 
-                @if($role->module_id==2 || $role->module_id==8 )
-                <input type="submit" class="btn btn-block btn-raised btn-info"><i class="glyphicon glyphicon-share-alt"></i>
-                @endif     
-                @if($role->module_id==3)
-                <input type="submit" class="btn btn-block btn-raised btn-warning"><i class="glyphicon glyphicon-share-alt"></i>
-                
-                @endif
-                @if($role->module_id==6 || $role->module_id==9)
-                <input type="submit" class="btn btn-block btn-raised btn-success"><i class="glyphicon glyphicon-share-alt"></i>
-                
-                @endif
-                @if($role->module_id==4 ||$role->module_id==5 ||$role->module_id==7 ||$role->module_id==10 || $role->module_id==11)
-                <input type="submit" class="btn btn-block btn-raised btn-success"><i class="glyphicon glyphicon-share-alt"></i>
-                
-                @endif
-
-                
-              
-              {{-- <span class="info-box-number">90<small>%</small></span> --}}
             </div>
             <!-- /.info-box-content -->
         </button>
+    </div>
         {!! Form::close() !!}
         @endforeach
     </div>
