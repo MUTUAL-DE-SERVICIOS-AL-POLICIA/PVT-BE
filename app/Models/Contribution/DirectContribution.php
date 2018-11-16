50ad68ed0c7fe7886430cf15328328357d822fd3
cechus <cechuscsc4@gmail.com>
2018-11-16 14:53:31 -0400

    refactorized tables and seeders

--- app/Models/Contribution/DirectContribution.php
+++ app/Models/Contribution/DirectContribution.php
@@ -6,17 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class DirectContribution extends Model
 {
-    protected $fillable = [
-        'user_id',
-        'affiliate_id',
-        'type',
-        'code',
-        'quotable',
-        'retirement_fund',
-        'mortuary_quota',
-        'mortuary_aid',
-        'subtotal',
-        'ipc',
-        'total'
-	];
+    public function contribution_processes()
+    {
+        return $this->hasMany("Muserpol\Models\Contribution\ContributionProcess");
+    }
+    public function affiliate()
+    {
+        return $this->belongsTo("Muserpol\Models\Affiliate");
+    }
+    public function contributor_type()
+    {
+        return $this->belongsTo("Muserpol\Models\Kinship");
+    }
+    public function procedure_state()
+    {
+        return $this->belongsTo("Muserpol\Models\ProcedureState");
+    }
+    public function procedure_modality()
+    {
+        return $this->belongsTo("Muserpol\Models\ProcedureModality");
+    }
 }
---
+++ database/seeds/DirectContributionsTableSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Muserpol\Models\ProcedureType;
+use Muserpol\Models\ProcedureModality;
+use Muserpol\Operation;
+
+class DirectContributionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $statuses = [
+            ['module_id' => 11, 'name' => 'Pago de Aportes Activos', 'second_name' => null],
+            ['module_id' => 11, 'name' => 'Pago de Aportes Pasivos', 'second_name' => null],
+        ];
+        foreach ($statuses as $status) {
+            // ProcedureType::create($status);
+        }
+        $statuses = [
+            ['procedure_type_id' => 6, 'name' => 'Agregado Policial', 'shortened' => ''],
+            ['procedure_type_id' => 6, 'name' => 'Baja Temporal', 'shortened' => ''],
+            ['procedure_type_id' => 6, 'name' => 'Comisión', 'shortened' => ''],
+            ['procedure_type_id' => 7, 'name' => 'Titular', 'shortened' => ''],
+            ['procedure_type_id' => 7, 'name' => 'Cónyuge', 'shortened' => ''],
+        ];
+        foreach ($statuses as $status) {
+            // ProcedureModality::create($status);
+        }
+        $statuses = [
+            // ['module_id' => 11, 'name' => 'Affiliate'],
+            // ['module_id' => 11, 'name' => 'ContributionProcess'],
+            ['module_id' => 11, 'name' => 'ContributionProcessSubmittedDocument'],
+            ['module_id' => 11, 'name' => 'Spouse'],
+            ['module_id' => 11, 'name' => 'Contribution'],
+            ['module_id' => 11, 'name' => 'AidContribution'],
+            ['module_id' => 11, 'name' => 'Reimbursement'],
+            ['module_id' => 11, 'name' => 'DirectContribution'],
+            ['module_id' => 11, 'name' => 'Voucher'],
+        ];
+        foreach ($statuses as $status) {
+            Operation::create($status);
+        }
+    }
+}
