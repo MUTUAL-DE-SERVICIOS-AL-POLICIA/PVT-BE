26922c1fe30e19349c1221fba74aad8b7533a679
Vrian7 <i-am-brian@hotmail.com>
2018-11-22 09:48:27 -0400

    arreglos documentos (#829)

--- app/Models/QuotaAidMortuary/QuotaAidLegalGuardian.php
+++ app/Models/QuotaAidMortuary/QuotaAidLegalGuardian.php
@@ -7,10 +7,11 @@ use Muserpol\Helpers\Util;
 class QuotaAidLegalGuardian extends Model
 {
     protected $table = "quota_aid_legal_guardians";
-    public function city_identity_card_id()    
+    public function city_identity_card()    
     {
         return $this->belongsTo('Muserpol\Models\City', 'city_identity_card_id');
     }
+    
     public function quota_aid_beneficiary()
     {
         return $this->belongsToMany('Muserpol\Models\QuotaAidMortuary\QuotaAidBeneficiary', 'quota_aid_beneficiary_legal_guardian', 'quota_aid_beneficiary_id', 'quota_aid_legal_guardian_id');
