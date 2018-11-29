9cc2ac4e1a08b76080865b48f7e6cadafca55aa6
cechus <cechuscsc4@gmail.com>
2018-11-29 10:30:21 -0400

    enhancements print

--- resources/views/contribution_process/print/quotation.blade.php
+++ resources/views/contribution_process/print/quotation.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body class="no-border text-xs">
     @for ($i = 0; $i < 3; $i++)
-        <div class="m-b-35" style="height:480px !important; border-bottom: 1px dashed #3c3c3c ">
+        <div class="m-b-20" style="height:490px !important; border-bottom: 1px dashed #3c3c3c ">
             <table class="w-100 ">
                 <tr>
                     <th class="w-20 text-left no-padding no-margins align-middle">
@@ -25,7 +25,7 @@
                         </div>
                     </th>
                     <th class="w-50 align-top">
-                        <div class="font-semibold uppercase leading-tight text-xs">
+                        <div class="font-semibold uppercase leading-tight text-sm">
                         {{ $institution ?? 'MUTUAL DE SERVICIOS AL POLICÍA "MUSERPOL"' }} <br>
                         {{ $direction ?? 'DIRECCIÓN DE BENEFICIOS ECONÓMICOS' }} <br>
                         {{ $unit ?? 'UNIDAD DE OTORGACIÓN DE FONDO DE RETIRO POLICIAL, CUOTA MORTUORIA Y AUXILIO MORTUORIO' }}
@@ -66,20 +66,20 @@
                 </div>
                 <table class="table-info w-100 m-b-5">
                     <thead class="bg-grey-darker">
-                        <tr class="font-medium text-white text-sm">
-                            <td class="px-15 text-left text-sm uppercase">
+                        <tr class="font-medium text-white text-xs">
+                            <td class="px-15 text-left uppercase">
                                 NOMBRE COMPLETO
                             </td>
-                            <td class="px-15 text-left text-sm uppercase w-30">
+                            <td class="px-15 text-left uppercase w-30">
                                 Cédula de identidad
                             </td>
-                            <td class="w-10 px-15 text-right text-sm uppercase">
+                            <td class="w-10 px-15 text-right uppercase">
                                 NUP
                             </td>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr class="text-sm">
+                        <tr class="text-xs">
                             <td class="text-left uppercase font-bold px-10 py-3">
                                 {{ $applicant->fullName() }}
                             </td>
@@ -95,11 +95,11 @@
                 <div class="font-bold uppercase m-b-5">
                     2.- PERIODOS
                 </div>
-                <div style="height: 125px !important; ">
+                <div style="height: 138px !important;">
                     @if ($direct_contribution->procedure_modality->procedure_type_id == 6)
                         <table class="table-info table-background">
                             <thead class="bg-grey-darker text-white">
-                                <tr class="text-xs text-center font-bold">
+                                <tr class="text-center text-sm">
                                     <td>
                                         Mes/Año
                                     </td>
@@ -122,7 +122,7 @@
                             </thead>
                             <tbody>
                                 @foreach ($contributions as $c)
-                                <tr class="courier">
+                                <tr class="courier text-base">
                                     <td class="px-5 text-left uppercase"> {{ Util::printMonthYear($c->month_year) }} </td>
                                     <td class="px-5 text-right"> {{ $c->quotable }} </td>
                                     <td class="px-5 text-right"> {{ $c->retirement_fund }} </td>
@@ -131,9 +131,9 @@
                                     <td class="px-5 text-right"> {{ $c->total }} </td>
                                 </tr>
                                 @endforeach
-                                <tr>
+                                <tr class="text-base">
                                     <td></td>
-                                    <td colspan="4" class="text-sm text-left px-10 ">
+                                    <td colspan="4" class="text-left px-10 ">
                                         Son: <span class="font-bold uppercase"> {{ Util::convertir($contribution_process->total) }} </span>
                                     </td>
                                     <td class="text-lg text-right courier font-bold">
@@ -194,19 +194,19 @@
                         </table>
                     @endif
                 </div>
-                <div style="width:500px;height:140px;" class="mx-auto border rounded">
-                        <table class="pt-50 ">
+                <div style="width:500px;height:120px;" class="mx-auto border rounded text-xs m-t-5">
+                        <table class="pt-45">
                             <tr>
-                                <td class="no-border text-center text-base w-100 align-bottom">
+                                <td class="no-border text-center w-100 align-bottom">
                                     <span class="font-bold">&nbsp;
                                         {{-- ---------------------------------------------------- --}}
                                     </span>
                                 </td>
                             </tr>
                             <tr>
-                                <td class="no-border text-center text-base w-100">
+                                <td class="no-border text-center w-100">
                                     <span class="font-bold block">{!! strtoupper($user->fullName()) !!}</span>
-                                    <div class="text-xs text-center uppercase" style="width: 450px; margin:0 auto; font-weight:100">{!! $user->position !!}</div>
+                                    <div class="text-center uppercase" style="width: 450px; margin:0 auto; font-weight:100">{!! $user->position !!}</div>
                                 </td>
                             </tr>
                         </table>
