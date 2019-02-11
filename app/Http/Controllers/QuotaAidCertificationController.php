90ca294aaa7ac1c8373b9d4eaf49f12eace0b253
Vrian7 <i-am-brian@hotmail.com>
2019-02-11 15:57:04 -0400

    solucion a impresion de archivo con mismo beneficio. close #915

--- app/Http/Controllers/QuotaAidCertificationController.php
+++ app/Http/Controllers/QuotaAidCertificationController.php
@@ -419,10 +419,11 @@ class QuotaAidCertificationController extends Controller
     }
     public function printFile($id)
     {
-        $affiliate = Affiliate::find($id);
+
+        $quota_aid = QuotaAidMortuary::where('id', $id)->where('code','NOT LIKE','%A')->get()->last();
+        $affiliate = Affiliate::find($quota_aid->affiliate_id);
         $role = Util::getRol();
 
-        $quota_aid = QuotaAidMortuary::where('affiliate_id', $affiliate->id)->where('code','NOT LIKE','%A')->get()->last();
         $next_area_code = QuotaAidCorrelative::where('quota_aid_mortuary_id', $quota_aid->id)->where('wf_state_id', 34)->first();
         $code = $quota_aid->code;
         $applicant = QuotaAidBeneficiary::where('type', 'S')->where('quota_aid_mortuary_id', $quota_aid->id)->first();
