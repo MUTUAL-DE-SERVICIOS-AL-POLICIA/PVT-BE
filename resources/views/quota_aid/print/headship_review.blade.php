23453c31db19caa327dc2aad4b520e61e4965817
Vrian7 <i-am-brian@hotmail.com>
2019-03-07 17:15:42 -0400

    certificacion CYA fixes

--- resources/views/quota_aid/print/headship_review.blade.php
+++ resources/views/quota_aid/print/headship_review.blade.php
@@ -12,7 +12,7 @@
                     </div>
                 </div>
                 <div class="text-center">
-
+                    {{ $i = 1 }}
                     {{ $correlative->code }}
                 </div>
             </div>
@@ -24,7 +24,7 @@
                         <thead class="bg-grey-darker">
                             <tr class="font-medium text-white text-sm uppercase">
                                 <td colspan='2' class="px-15 text-left">
-                                    1. DATOS DEL TITULAR
+                                    {{ $i++ }}. DATOS DEL TITULAR
                                 </td>
                             </tr>
                         </thead>
@@ -75,13 +75,72 @@
                         </tbody>
                     </table>
                 </div>
+                @if($quota_aid->procedure_modality_id == 14 || $quota_aid->procedure_modality_id == 15)
                 <br>
+                <div class="inline-block align-top w-100">
+                        <table class="table-info w-100 m-b-10">
+                            <thead class="bg-grey-darker">
+                                <tr class="font-medium text-white text-sm uppercase">
+                                    <td colspan='2' class="px-15 text-left">
+                                        {{ $i++ }}. DATOS DEL CAUSANTE
+                                    </td>
+                                </tr>
+                            </thead>
+                            <tbody class="table-striped">
+                                <tr class="text-sm">
+                                    <td class="w-40 text-left px-10 py-3 uppercase">nombres y apellidos</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3"> {{ Util::fullName($spouse) }} </td>
+                                </tr>
+                                <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">cédula de identidad</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">{!! $spouse->identity_card !!} {{$spouse->city_identity_card->first_shortened ?? ''}}</td>
+                                </tr>
+                                <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">fecha de Nacimiento</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">{{ $spouse->getBirthDate() }}</td>
+                                </tr>
+                                <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">Matricula</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">{{ $spouse->registration }}</td>
+                                </tr>
+                                <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">Estado Civil</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">{{ $spouse->getCivilStatus() }}</td>
+                                </tr>
+                                <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">Edad</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">{{ Util::calculateAgeYears($spouse->birth_date, $spouse->date_death) }}</td>
+                                </tr>
+                                {{-- <tr class="text-sm">
+                                    <td class="text-left px-10 py-3 uppercase">Dirección Domicilio</td>
+                                    <td class="text-left uppercase font-bold px-5 py-3">
+                                        {{ $spouse->address()->first() ? $spouse->address()->first()->city->name.',' ?? '' : '' }}
+                                        {{ $spouse->address()->first() ? 'Zona. '.$spouse->address()->first()->zone.',' : '' }}
+                                        {{ $spouse->address()->first() ? 'Av. Calle '.$spouse->address()->first()->street.',' : '' }}
+                                        {{ $spouse->address()->first() ? '#'.$spouse->address()->first()->number_address.'.' : '' }}
+                                    </td>
+                                </tr> --}}
+                                @if ($spouse->date_death)
+                                    <tr class="text-sm">
+                                        <td class="text-left px-10 py-3 uppercase">FECHA DE FALLECIMIENTO</td>
+                                        <td class="text-left uppercase font-bold px-5 py-3">{{ $spouse->getDateDeath() }}</td>
+                                    </tr>
+                                    <tr class="text-sm">
+                                        <td class="text-left px-10 py-3 uppercase">CAUSA DE FALLECIMIENTO</td>
+                                        <td class="text-left uppercase font-bold px-5 py-3">{{ $spouse->reason_death }}</td>
+                                    </tr>
+                                @endif
+                            </tbody>
+                        </table>
+                    </div>
+                    @endif
+                    <br>
                 <div class="inline-block align-top w-100">
                     <table class="table-info w-100 m-b-10">
                         <thead class="bg-grey-darker">
                             <tr class="font-medium text-white text-sm uppercase">
                                 <td colspan='2' class="px-15 text-left">
-                                    2. DATOS INSTITUCIONALES
+                                        {{ $i++ }}. DATOS INSTITUCIONALES
                                 </td>
                             </tr>
                         </thead>
@@ -118,7 +177,7 @@
                             <thead class="bg-grey-darker">
                                 <tr class="font-medium text-white text-sm uppercase">
                                     <td colspan='2' class="px-15 text-left">
-                                        2. DATOS INSTITUCIONALES
+                                        {{ $i++ }}. PROCEDIMIENTOS REALIZADOS CORRECTAMENTE
                                     </td>
                                 </tr>
                             </thead>
