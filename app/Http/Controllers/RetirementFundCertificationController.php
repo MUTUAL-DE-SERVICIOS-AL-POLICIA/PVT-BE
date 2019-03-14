4beb99713d8650053270a0c6db7d7a7971662fdc
Alejandro Erick Guisbert Flor <alerick@MacBook-Air-de-Alejandro.local>
2019-03-14 16:45:11 -0400

    update resolution

--- app/Http/Controllers/RetirementFundCertificationController.php
+++ app/Http/Controllers/RetirementFundCertificationController.php
@@ -1936,7 +1936,7 @@ class RetirementFundCertificationController extends Controller
         <i>“I. La Comisión de Beneficios Económicos es la instancia técnica legal que realiza el procedimiento
         administrativo para la otorgación del beneficio de Fondo de Retiro Policial Solidario. Es designada
         mediante Resolución Administrativa de la Dirección General Ejecutiva de la MUSERPOL”</i>. Por consiguiente,
-        la Resolución Administrativa Nº 004/2019 del 7 de enero de 2019, conforma la Comisión de Beneficios Económicos,
+        la Resolución Administrativa Nº 011/2019 del 14 de marzo de 2019, conforma la Comisión de Beneficios Económicos,
         en cumplimiento al Reglamento.
         <br><br>';
 
