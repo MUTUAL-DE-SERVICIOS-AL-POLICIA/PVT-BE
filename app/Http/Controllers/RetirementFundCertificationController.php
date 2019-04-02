8bb221ca539e130cdeecb2d3bdf122b709772b9b
alerick <aleguis@icloud.com>
2019-04-02 10:32:32 -0400

    fix print calif

--- app/Http/Controllers/RetirementFundCertificationController.php
+++ app/Http/Controllers/RetirementFundCertificationController.php
@@ -756,9 +756,9 @@ class RetirementFundCertificationController extends Controller
             //     $pages[] =\View::make('ret_fun.print.qualification_data_ret_fun_availability', self::printDataQualificationRetFunAvailability($id, false))->render();
             // }
         }
-        if ($retirement_fund->total_ret_fun >= 0) {
-            $pages[] =\View::make('ret_fun.print.qualification_step_data', self::printDataQualification($id, false))->render();
-        }
+
+        $pages[] =\View::make('ret_fun.print.qualification_step_data', self::printDataQualification($id, false))->render();
+        
         $pages[] =\View::make('ret_fun.print.beneficiaries_qualification', self::printBeneficiariesQualification($id, false))->render();
         if ($affiliate->hasAvailability()) {
             if ($retirement_fund->total_availability > 0) {
@@ -768,9 +768,9 @@ class RetirementFundCertificationController extends Controller
             //     $pages[] =\View::make('ret_fun.print.qualification_data_ret_fun_availability', self::printDataQualificationRetFunAvailability($id, false))->render();
             // }
         }
-        if ($retirement_fund->total_ret_fun >= 0) {
-            $pages[] =\View::make('ret_fun.print.qualification_step_data', self::printDataQualification($id, false))->render();
-        }
+        
+        $pages[] =\View::make('ret_fun.print.qualification_step_data', self::printDataQualification($id, false))->render();
+        
         $pages[] =\View::make('ret_fun.print.beneficiaries_qualification', self::printBeneficiariesQualification($id, false))->render();
 
         if (!$affiliate->selectedContributions() > 0 && ! $affiliate->globalPayRetFun()  && $retirement_fund->procedure_modality->procedure_type->id == 2 ){
