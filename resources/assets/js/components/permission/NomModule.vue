c9e57a31c3f11fe1199b9bb3dab1fb6a38971e0d
cechus <cechuscsc4@gmail.com>
2019-04-28 04:21:40 -0400

    enhancements authorizations

--- resources/assets/js/components/permission/NomModule.vue
+++ resources/assets/js/components/permission/NomModule.vue
@@ -1,164 +1,108 @@
 <script>
 export default {
- 
-  props:[
-			'roles',
-      'permissions',
-      'operations',
-      'role_permissions',
-      'actions'
-		],
+  props: ["roles", "permissions", "operations", "role_permissions", "actions"],
   data() {
     return {
       module_id: null,
       roles_list: this.roles,
-      permissions_list: this.permissions,
-      operations_list: this.operations,
-      actions_list: this.actions,
-      role_permissions_list: this.role_permissions,
+      permissions_list: [],
+      operations_list: [],
+      role_permissions_list: [],
       opertaion_list_role: null,
       role: null,
       list_to_send: [],
       sw: false,
-    }
+      loadingButton: false
+    };
   },
   computed: {
-     roles_module: function(){
-        console.log('ingresndo funcion hdp');
-        var lista =[];
-        if(this.module_id)
-        {
-          for (var i = this.roles_list.length - 1; i >= 0; i--) {
-            console.log(this.roles_list[i].module_id);
-            if(this.roles_list[i].module_id==this.module_id){
-              lista.push(this.roles_list[i]);
-            }
-          }
-          console.log('imprimiendo los seleccionados ');
-          console.log(lista);
-
-        }
-        return lista;
-     },
-     permissions_role: function(){
-        
-        var lista=[];
-        if(this.role)
-        {
-          for (var i = this.permissions_list.length - 1; i >= 0; i--) {
-            console.log(this.permissions_list[i]);
-            // if(this.role.id = )
-          }
-        }
-     },
-     getOperationPermissionList: function()
-     {
-        this.list_to_send =[];
-        if(this.role)
-        {
-          console.log('refrescando this.list_to_send CyA');
-          for (var i = this.operations_list.length - 1; i >= 0; i--) {
-          
-            if(this.operations_list[i].module_id == this.module_id)
-            {
-              var obj = {operation_id: this.operations_list[i].id , name: this.operations_list[i].name};
-              for (var j = 0; j < this.actions_list.length; j++) {
-        
-                obj[this.actions_list[j].name] = this.CheckPermission(this.operations_list[i],this.actions_list[j].id); 
-              }
-              // console.log(obj);
-              this.list_to_send.push(obj);
-            }
-          }
-        }
-
-        return this.list_to_send;
-     }
-
-  },
-  methods: {
-      SelectRol: function(role)
-      { 
-         this.role = role;
-         // var obj={operation_id: 1 , name: 'a'};
-         // console.log(obj);
-         // for (var i = this.actions.length - 1; i >= 0; i--) {
-           
-         //   obj[this.actions[i].name] = this.CheckPermissionCreate(this.roles_list[1]);
-         // }
-         // console.log(obj);
-         // console.log(role);
-      },
-      selectAll: function(){
-        
-        this.sw = !this.sw;
-        for (var i = 0; i < this.list_to_send.length; i++) {
-
-         if(this.sw){
-              this.list_to_send[i].create =true;
-              this.list_to_send[i].read =true;
-              this.list_to_send[i].update =true;
-              this.list_to_send[i].delete =true;
-              this.list_to_send[i].print =true; 
-         }else
-         {
-              this.list_to_send[i].create =false;
-              this.list_to_send[i].read =false;
-              this.list_to_send[i].update =false;
-              this.list_to_send[i].delete =false;
-              this.list_to_send[i].print =false; 
-         }
-
-         
-        }
-      },
-      CheckPermission: function(operation,action_id)
-      {
-        // console.log('CheckPermission');
-        var permissions=[];
-        var result = false;
-        if(this.role)
-        {
-          for (var i = this.permissions_list.length - 1; i >= 0; i--) {
-            if(this.permissions_list[i].operation_id ==operation.id && this.permissions_list[i].action_id == action_id)
-            {
-              permissions.push(this.permissions_list[i]);
-            }
-          }
-          // console.log("size: "+permissions.length)
-          // console.log(permissions);
-          // for (var i = permissions.length - 1; i >= 0; i--) {
-          //   console.log(permissions[i]);
-          // }
-          for (var i = 0; i < this.role_permissions_list.length; i++) {
-            for (var j = 0; j < permissions.length; j++) {
-              if(permissions[j].id == this.role_permissions_list[i].permission_id && this.role.id == this.role_permissions_list[i].role_id)
-              {
-                result= true;
-              }
-            }
-          }
-
-        }
-        return result;
-      },
-      update: function () { 
-
-               var lista = this.list_to_send;
-               var data = {module_id:this.module_id ,role_id: this.role.id, permissions_list: lista };
-               axios.post('/permission', data )
-                    .then(function (resp) {
-                        // this.$router.push({path: '/'});
-                        console.log(resp);
-                        flash('Informacion Actualizada');
-                    })
-                    .catch(function (resp) {
-                        console.log(resp);
-                        flash('Error: '+resp.message,'error');
-                    });
+    roles_module: function() {
+      return this.roles_list.filter(r => r.module_id == this.module_id);
+    },
+    getOperationPermissionList: function() {
+      this.list_to_send = [];
+      if (this.role) {
+        this.operations_list.forEach(operation => {
+          var obj = {
+            operation_id: operation.id,
+            name: operation.name
+          };
+          this.actions.forEach(action => {
+            obj[action.name] = this.CheckPermission(operation, action.id);
+          });
+          this.list_to_send.push(obj);
+        });
       }
 
-
+      return this.list_to_send;
+    }
+  },
+  methods: {
+    SelectRol: function(role) {
+      this.role = role;
+      this.operations_list = this.operations.filter(
+        o => o.module_id == this.module_id
+      );
+      this.permissions_list = this.permissions.filter(
+        p => p.module_id == this.module_id
+      );
+      this.role_permissions_list = this.role_permissions.filter(
+        r => r.role_id == this.role.id
+      );
+    },
+    selectAll: function() {
+      this.sw = !this.sw;
+      for (var i = 0; i < this.list_to_send.length; i++) {
+        if (this.sw) {
+          this.list_to_send[i].create = true;
+          this.list_to_send[i].read = true;
+          this.list_to_send[i].update = true;
+          this.list_to_send[i].delete = true;
+          this.list_to_send[i].print = true;
+        } else {
+          this.list_to_send[i].create = false;
+          this.list_to_send[i].read = false;
+          this.list_to_send[i].update = false;
+          this.list_to_send[i].delete = false;
+          this.list_to_send[i].print = false;
+        }
+      }
+    },
+    CheckPermission: function(operation, action_id) {
+      var permissions = [];
+      var result = false;
+      if (this.role) {
+        this.permissions_list.forEach(p => {
+          if (p.operation_id == operation.id && p.action_id == action_id) {
+            permissions.push(p.id);
+          }
+        });
+        this.role_permissions_list.forEach(r => {
+          if (permissions.includes(r.permission_id)) {
+            result = true;
+          }
+        });
+      }
+      return result;
+    },
+    async update() {
+      this.loadingButton = true;
+      var lista = this.list_to_send;
+      var data = {
+        module_id: this.module_id,
+        role_id: this.role.id,
+        permissions_list: lista
+      };
+      await axios
+        .post("/permission", data)
+        .then(function(resp) {
+          flash("Informacion Actualizada");
+        })
+        .catch(function(resp) {
+          flash("Error: " + resp.message, "error");
+        });
+      this.loadingButton = false;
+    }
   }
 };
 </script>
