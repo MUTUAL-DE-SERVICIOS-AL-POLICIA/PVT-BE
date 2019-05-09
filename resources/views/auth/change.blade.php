2bc5e4b3aebbe30bd1d4bd3bdc6e44cc19443d90
Daniel Jimenez Jerez <djimenezjerez@gmail.com>
2019-05-09 16:04:32 -0400

    Minified role selection view

--- resources/views/auth/change.blade.php
+++ resources/views/auth/change.blade.php
@@ -4,138 +4,67 @@
 
 @section('content')
 <div class="row">
-    <h1 class="text-navy text-center"><b>Plataforma Virtual de Trámites</b></h1>
+  <h1 class="text-navy text-center"><b>Plataforma Virtual de Trámites</b></h1>
 </div>
 <div class="wrapper wrapper-content animated fadeInRight">
-    <div class="row text-center">
-        @php
-            $count = count($roles);
-            $style = "";
-        @endphp
-        @if($count == 1)
-            @php
-                $style = "<div class='col-md-3 col-md-offset-4'>";
-            @endphp
-        @elseif($count == 2)
-            @php
-                $style = "<div class='col-md-3 col-md-offset-2'>";
-            @endphp
-        @elseif($count == 3)
-            @php
-                $style = "<div class='col-md-3 col-md-offset-1'>";
-            @endphp
-        @else
-            @php
-                $style = "<div class='col-md-3'>";
-            @endphp
-        @endif
-        @foreach($roles as $role)
-        {!! Form::open(['url' => 'postchangerol', 'role' => 'form']) !!}
-                @php
-                    echo $style;    
-                @endphp
-                @if($role->module_id==1)
-                <button class="widget style1 red-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                    <div class="row">
-                        <div class="col-xs-3">
-                            <i class="glyphicon glyphicon-hdd fa-4x"></i>
-                        </div>
-                        <div class="col-xs-9">
-                            <span>
-                                <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                            </span>
-                @endif
-                @if($role->module_id==2 || $role->module_id==8 )
-                <button class="widget style1 lazur-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-puzzle-piece fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif     
-                @if($role->module_id==3)
-                <button class="widget style1 yellow-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="glyphicon glyphicon-piggy-bank fa-4x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
-                @if($role->module_id==6 || $role->module_id==9)
-                <button class="widget style1 blue-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-money fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
-                @if($role->module_id==4 ||$role->module_id==5 )
-                <button class="widget style1 blue-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-heartbeat fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
-                @if($role->module_id==7)
-                <button class="widget style1 blue-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa fa-balance-scale fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
-                @if($role->module_id==10)
-                <button class="widget style1 green-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa  fa-map fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
-                @if($role->module_id == 11)
-                <button class="widget style1 green-bg px-1 text-center" style="height: 100px; width: 100%; border: none">
-                        <div class="row">
-                            <div class="col-xs-3">
-                                <i class="fa  fa-map fa-5x"></i>
-                            </div>
-                            <div class="col-xs-9">
-                                <span>
-                                    <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
-                                </span>
-                @endif
+  <div class="row text-center">
+    @php
+      $count = count($roles);
+      $style = "";
+      $icons = [
+        1 => ['color' => 'red-bg', 'icon' => 'glyphicon glyphicon-hdd fa-4x'],
+        2 => ['color' => 'lazur-bg', 'icon' => 'fa fa-puzzle-piece fa-5x'],
+        3 => ['color' => 'yellow-bg', 'icon' => 'glyphicon glyphicon-piggy-bank fa-4x'],
+        4 => ['color' => 'blue-bg', 'icon' => 'fa fa-heartbeat fa-5x'],
+        6 => ['color' => 'blue-bg', 'icon' => 'fa fa-money fa-5x'],
+        7 => ['color' => 'blue-bg', 'icon' => 'fa fa-balance-scale fa-5x'],
+        10 => ['color' => 'green-bg', 'icon' => 'fa fa-map fa-5x'],
+      ];
+      $icons[8]=$icons[2];
+      $icons[9]=$icons[6];
+      $icons[5]=$icons[4];
+      $icons[11]=$icons[10];
 
-                <input type="hidden" name="rol_id" value={!! $role->id !!}>
-                <b>
-                <span class="info-box-text"><p style="font-size: 1em">{!! $role->module->name !!}</p>  </span>
-                </b>
-                <br>
-            <!-- /.info-box-content -->
-        </div>
-        </div>
+      switch($count) {
+        case(1):
+          $style = "<div class='col-md-3 col-md-offset-4'>";
+          break;
+        case(2):
+          $style = "<div class='col-md-3 col-md-offset-2'>";
+          break;
+        case(3):
+          $style = "<div class='col-md-3 col-md-offset-1'>";
+          break;
+        default:
+          $style = "<div class='col-md-3'>";
+      }
+    @endphp
+    @foreach($roles as $role)
+    {!! Form::open(['url' => 'postchangerol', 'role' => 'form']) !!}
+      @php
+        echo $style;
+      @endphp
+        <button class="widget style1 {{$icons[$role->module_id]['color']}} px-1 text-center" style="height: 100px; width: 100%; border: none">
+          <div class="row">
+            <div class="col-xs-3">
+              <i class="{{$icons[$role->module_id]['icon']}}"></i>
+            </div>
+            <div class="col-xs-9">
+              <span>
+                <b><p style="font-size: 1.5em;">{{$role->name}}</p></b>
+              </span>
+              <input type="hidden" name="rol_id" value={!! $role->id !!}>
+              <b>
+              <span class="info-box-text"><p style="font-size: 1em">{!! $role->module->name !!}</p>  </span>
+              </b>
+              <br>
+              <!-- /.info-box-content -->
+            </div>
+          </div>
         </button>
-    </div>
-        {!! Form::close() !!}
-        @endforeach
-    </div>
+      </div>
+    {!! Form::close() !!}
+    @endforeach
+  </div>
 </div>
- 
 @endsection
