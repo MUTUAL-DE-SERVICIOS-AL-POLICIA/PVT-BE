685d68a59eb9858742c8ea28b0b8bc3140a9407b
cechus <cechuscsc4@gmail.com>
2019-07-17 18:23:33 -0400

    enhacements list

--- app/Imports/EcoComImportPagoFuturo.php
+++ app/Imports/EcoComImportPagoFuturo.php
@@ -37,43 +37,43 @@ class EcoComImportPagoFuturo implements ToCollection
                 // ->where('eco_com_applicants.identity_card', $ci)
                 ->first();
             if ($eco_com) {
-                if (!$eco_com->hasObservationType($pago_futuro_id)) {
-                    $eco_com->observations()->save($observation, [
-                        'user_id' => Auth::user()->id,
-                        'date' => now(),
-                        'message' => "Observación Importada",
-                        'enabled' => false
-                    ]);
-                    $eco_com->observations()->save($eco_com, [
-                        'user_id' => Auth::user()->id,
-                        'date' => now(),
-                        'message' => "Observación Importada",
-                        'enabled' => false
-                    ]);
-                }
-
-                $subtotal = $eco_com->aps_total_cc + $eco_com->aps_total_fsa + $eco_com->aps_total_fs + $eco_com->aps_disability + $eco_com->aps_total_death;
-                $total = $subtotal * 2.03 / 100;
-                $aux = $total * 6;
-                $discount_type = DiscountType::findOrFail(7);
-                if ($eco_com->discount_types->contains($discount_type->id)) {
-                    $eco_com->discount_types()->updateExistingPivot($discount_type->id, ['amount' => $aux, 'date' => now()]);
+                if (!Util::isDoblePerceptionEcoCom($ci)) {
+                    if (!$eco_com->hasObservationType($pago_futuro_id)) {
+                        $eco_com->observations()->save($observation, [
+                            'user_id' => Auth::user()->id,
+                            'date' => now(),
+                            'message' => "Observación Importada",
+                            'enabled' => true
+                        ]);
+                        logger("observacion creada");
+                        // $subtotal = $eco_com->aps_total_cc + $eco_com->aps_total_fsa + $eco_com->aps_total_fs + $eco_com->aps_disability + $eco_com->aps_total_death;
+                        $eco_com->calculateTotalRentAps();
+                        $total_rent = $eco_com->total_rent;
+                        if ($total_rent > 0) {
+                            $total = $total_rent * 2.03 / 100;
+                            $aux = $total * 6;
+                            $discount_type = DiscountType::findOrFail(7);
+                            if ($eco_com->discount_types->contains($discount_type->id)) {
+                                $eco_com->discount_types()->updateExistingPivot($discount_type->id, ['amount' => $aux, 'date' => now()]);
+                            } else {
+                                $eco_com->discount_types()->save($discount_type, ['amount' => $aux, 'date' => now()]);
+                            }
+                            logger("discount creado");
+                        }else{
+                            logger("no tiene total rent");
+                        }
+                    }
+                    // if (!Util::isDoblePerceptionEcoCom($ci)) {
+                    //     $discount_type = DiscountType::findOrFail($pago_futuro_id);
+                    //     if ($eco_com->discount_types->contains($discount_type->id)) {
+                    //         $eco_com->discount_types()->updateExistingPivot($discount_type->id, ['amount' => $amount, 'date' => now()]);
+                    //     } else {
+                    //         $eco_com->discount_types()->save($discount_type, ['amount' => $amount, 'date' => now()]);
+                    //     }
+                    //     $found++;
                 } else {
-                    $eco_com->discount_types()->save($discount_type, ['amount' => $aux, 'date' => now()]);
+                    logger("sii doble" . $ci);
                 }
-
-
-                // if (!Util::isDoblePerceptionEcoCom($ci)) {
-                //     $discount_type = DiscountType::findOrFail($pago_futuro_id);
-                //     if ($eco_com->discount_types->contains($discount_type->id)) {
-                //         $eco_com->discount_types()->updateExistingPivot($discount_type->id, ['amount' => $amount, 'date' => now()]);
-                //     } else {
-                //         $eco_com->discount_types()->save($discount_type, ['amount' => $amount, 'date' => now()]);
-                //     }
-                //     $found++;
-                // }else{
-                //     logger("sii".$ci);
-                //  }
             } else {
                 $not_found->push($ci);
             }
