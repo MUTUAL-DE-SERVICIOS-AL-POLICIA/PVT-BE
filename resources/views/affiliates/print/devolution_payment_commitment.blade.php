bf8ab68ba616253777498ef5002f66a98896f5ea
lore <darlin.lore@gmail.com>
2019-07-26 18:41:02 -0400

    add totals report devolution print

--- resources/views/affiliates/print/devolution_payment_commitment.blade.php
+++ resources/views/affiliates/print/devolution_payment_commitment.blade.php
@@ -35,7 +35,7 @@
         <thead class="bg-grey-darker">
             <tr class="font-medium text-white">
                 <td class="px-15 py text-center">
-                    Gestión
+                    GESTION
                 </td>
                 <td class="px-15 py text-center">
                     MONTO ADEUDADO
@@ -43,13 +43,14 @@
             </tr>
         </thead>
         <tbody>
+        @php($sum = 0)
         @foreach ($duess as $dd)
-
             @foreach ($dues as $d)
              @if($dd== $d->id)
+             @php($sum += $d->amount)
             <tr>
                 <td class="text-left uppercase px-10 py-3">
-                    {{ $d->eco_com_procedure->getTextName()}} -  {{ $d->id}} - {{ $d->duess }}
+                    {{ $d->eco_com_procedure->getTextName()}}
                 </td>
                 <td class="text-right px-10 py-3">
                     {{ Util::formatMoney($d->amount) }}
@@ -66,9 +67,9 @@
     <div class="w-80 mx-auto m-t-20">
         <table class=" w-100 ">
             <tr>
-                <td class="border uppercase font-bold p-5">Deuda Total</td>
+                <td class="border uppercase font-bold p-5">DEUDA TOTAL</td>
                 <td class="border font-bold  p-5"> Bs.
-                    {{ Util::formatMoney($devolution->total) }}({{ Util::convertir($devolution->total) }} BOLIVIANOS)
+                    {{ Util::formatMoney($sum) }}({{ Util::convertir($sum) }} BOLIVIANOS)
                 </td>
             </tr>
         </table>
