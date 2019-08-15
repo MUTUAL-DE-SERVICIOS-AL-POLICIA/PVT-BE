ce3cce66769c2749fc7561b779ca17ed520d6f5b
cechus <cechuscsc4@gmail.com>
2019-08-15 00:39:07 -0400

    added pago viuda por unica vez, bug fixed print devolutions

--- app/Models/EconomicComplement/EconomicComplement.php
+++ app/Models/EconomicComplement/EconomicComplement.php
@@ -690,6 +690,12 @@ class EconomicComplement extends Model
                 $beneficiary = $this->eco_com_legal_guardian;
             }
         }
+        if ($this->is_paid_spouse) {
+            $spouse = $this->affiliate->spouse->first();
+            if ($spouse) {
+                $beneficiary = $spouse;
+            }
+        }
         return $beneficiary;
     }
     public function isLagging()
