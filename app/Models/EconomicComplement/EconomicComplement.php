ab8264cdecc70eb306c569516c59164d4841a325
alerick <aleguisf@gmail.com>
2020-07-23 11:21:53 -0400

    Update EconomicComplement.php

--- app/Models/EconomicComplement/EconomicComplement.php
+++ app/Models/EconomicComplement/EconomicComplement.php
@@ -440,11 +440,11 @@ class EconomicComplement extends Model
     {
         if ($id == 1) {
             $collect = collect([1,2,3,4,5,6,7,8,10,11,12,13]);
-            $query->where('affiliates.sigep_status', 'LIKE', "ACTIVO")
+            $query->where('affiliates.sigep_status', 'LIKE', 'ACTIVO')
                 ->whereIn('affiliates.financial_entity_id',$collect)
                 ->where('affiliates.account_number', '>', 0);
         } elseif ($id == 2) {
-            $query->where('affiliates.account_number', null);
+            $query->where('affiliates.sigep_status', 'NOT LIKE', 'ACTIVO');
         } 
         return $query;
     }
@@ -452,7 +452,7 @@ class EconomicComplement extends Model
     public function scopeIndirectPayment($query)
     {          
         $query->leftJoin('affiliates', 'economic_complements.affiliate_id', '=', 'affiliates.id') 
-            ->where('affiliates.account_number', null);
+            ->where('affiliates.sigep_status', 'NOT LIKE', 'ACTIVO');
         return $query;
     }
 
