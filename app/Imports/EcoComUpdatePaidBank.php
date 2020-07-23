3539d8a52c1b1c7fb91ef21cd6e697c107076e75
alerick <aleguisf@gmail.com>
2020-07-23 10:57:14 -0400

    Update EcoComUpdatePaidBank.php

--- app/Imports/EcoComUpdatePaidBank.php
+++ app/Imports/EcoComUpdatePaidBank.php
@@ -37,11 +37,28 @@ class EcoComUpdatePaidBank implements ToCollection
 
          foreach ($rows as $row) {
             
-            $nup = strval($row[0]);
+            $nup = strval($row[0]);   
+            $affiliate = Affiliate::where('id', $nup)->first();
+            if ($affiliate) {
+                $eco_coms = $affiliate->economic_complements()->where('eco_com_procedure_id', $current_procedure)->get();            
+                foreach ($eco_coms as $eco) {
+                    if ( $eco->eco_com_state_id == 16) {
+                        $eco->eco_com_state_id = 25;
+                        $eco->save();
+                        $found++;
+                    }else{
+                        $not_found_t->push($nup);
+                    }
+                }                  
+            }else{
+                $not_found->push($nup);
+            }
+            /*
+            
             $sigep_status = strval($row[14]);
             $financial_entity_id = strval($row[15]) ? FinancialEntity::where('name', strval($row[15]))->first()->id : null;
-            $account_number = strval($row[16]) ? strval($row[16]): null;          
-            $affiliate = Affiliate::where('id', $nup)->first();
+            $account_number = strval($row[16]) ? strval($row[16]): null;  
+            
             if ($affiliate) {
                 if (in_array($sigep_status, $status)) {
                     $affiliate->account_number = $account_number;
@@ -56,11 +73,13 @@ class EcoComUpdatePaidBank implements ToCollection
                 }
             }else {
                 $not_found_t->push($nup);
-            }
+            }*/
             
             
             
-            /* $nup = strval($row[0]); 
+            /* 
+            
+            $nup = strval($row[0]); 
             $affiliate = Affiliate::where('id', $nup)->first();     
             if ($affiliate) {
                 $eco_coms = $affiliate->economic_complements()->where('eco_com_procedure_id', $current_procedure)->get();            
