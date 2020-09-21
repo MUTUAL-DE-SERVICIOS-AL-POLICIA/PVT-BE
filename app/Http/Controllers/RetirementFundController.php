daf5d432ce2939022400ba075586995c519b8285
alerick <aleguisf@gmail.com>
2020-09-21 19:26:39 -0400

    Update RetirementFundController.php

--- app/Http/Controllers/RetirementFundController.php
+++ app/Http/Controllers/RetirementFundController.php
@@ -640,7 +640,7 @@ class RetirementFundController extends Controller
             }
         }
         $applicant = RetFunBeneficiary::where('type', 'S')->where('retirement_fund_id', $retirement_fund->id)->first();
-        Log::info($applicant);
+
         $beneficiary_avdisor = RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first();
 
         if (isset($beneficiary_avdisor->id))
