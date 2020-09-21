c385f9825bab6a68c8c521867b3374b3ca9f68e3
alerick <aleguisf@gmail.com>
2020-09-21 15:41:11 -0400

    Update RetirementFundController.php

--- app/Http/Controllers/RetirementFundController.php
+++ app/Http/Controllers/RetirementFundController.php
@@ -641,12 +641,12 @@ class RetirementFundController extends Controller
         }
         $applicant = RetFunBeneficiary::where('type', 'S')->where('retirement_fund_id', $retirement_fund->id)->first();
 
-        $beneficiary_avdisor = RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first();
+        /*$beneficiary_avdisor = RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first();
 
         if (isset($beneficiary_avdisor->id))
             $advisor = RetFunAdvisor::find($beneficiary_avdisor->ret_fun_advisor_id);
-        else
-            $advisor = new RetFunAdvisor();
+        else*/
+        $advisor = new RetFunAdvisor();
 
         $beneficiary_guardian = RetFunLegalGuardianBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first();
 
