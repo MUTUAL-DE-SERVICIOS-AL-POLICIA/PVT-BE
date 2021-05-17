b43a497282180579a5614186e2d58247e16445a5
alerick <aleguisf@gmail.com>
2021-05-17 11:17:25 -0400

    Update EconomicComplementController.php

--- app/Http/Controllers/API/EconomicComplementController.php
+++ app/Http/Controllers/API/EconomicComplementController.php
@@ -256,15 +256,14 @@ class EconomicComplementController extends Controller
 
             $pages[] = \View::make('eco_com.print.reception', $data)->render();
 
-            $pdf = \App::make('snappy.pdf.wrapper');
-            $pdf->loadHTML($pages)->setOption('encoding', 'utf-8')
+            $pdf = \App::make('snappy.pdf');
+            $pdf->generateFromHtml($pages)->setOption('encoding', 'utf-8')
             ->setOption('margin-bottom', '23mm')
-            ->setOption('footer-html', $footerHtml)
-            ->stream("Reception " . $economic_complement->id . '.pdf');
+            ->setOption('footer-html', $footerHtml);
 
             return response()->make($pdf, 200, [
                 'Content-Type' => 'application/pdf',
-                'Content-Disposition' => 'inline; filename="'.$pdf.'"'
+                'Content-Disposition' => 'inline; filename="'$economic_complement->id.$pdf.'"'
             ]);
 
         } else {
