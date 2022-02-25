0b2b0aa1133b03e779124398101a73ba92a3461d
Ale Guis F <aleguisf@gmail.com>
2022-02-25 11:30:38 -0400

    bug fixes show requirements eco com

--- app/Http/Controllers/AffiliateSubmittedDocumentsController.php
+++ app/Http/Controllers/AffiliateSubmittedDocumentsController.php
@@ -15,37 +15,13 @@ class AffiliateSubmittedDocumentsController extends Controller
         if ($request->reception_type_id == 2) {
             $procedure_requirements_original = ProcedureRequirement::with('procedure_document')->where('procedure_modality_id', $request->procedure_modality_id)->where('number', '<>', 0)->orderBy('number')->get();
             $additional_requirements = ProcedureRequirement::with('procedure_document')->where('procedure_modality_id', $request->procedure_modality_id)->where('number', '=', 0)->orderBy('number')->get();
-            $procedure_requirements = ProcedureRequirement::where('procedure_modality_id', $request->procedure_modality_id)
-                ->where('number', '<>', 0)
-                ->select(DB::raw("number, string_agg(procedure_document_id::text, ',') as procedure_document_ids"))
-                ->groupBY('number')
-                ->orderBy('number')
-                ->get();
-            $collect = collect([]);
-            foreach ($procedure_requirements as $p) {
-                foreach (explode(',', $p->procedure_document_ids) as $d) {
-                    foreach ($affiliate->submitted_documents()->where('status', true)->get() as $a) {
-                        /*if ($a->procedure_document_id != 269)
-                        {
-                            if ($a->procedure_document_id != 271)
-                            {*/
-                                if ($a->procedure_document_id == $d && $affiliate->submitted_documents()->where('status', true)->count() > 2) {
-                                    $collect->push($p->number);
-                                    break;
-                                }
-                            /*}
-                        }*/
-                    }
-                }
-            }
+            
             $requirements = collect([]);
             foreach ($procedure_requirements_original as $po) {
-                if (!$collect->contains($po->number)) {
                     $po->background = "";
                     $po->status = false;
                     $po->number = "N" . $po->number;
                     $requirements->push($po);
-                }
             }
             $requirements = $requirements->groupBy('number')->toArray();
             $data = [
