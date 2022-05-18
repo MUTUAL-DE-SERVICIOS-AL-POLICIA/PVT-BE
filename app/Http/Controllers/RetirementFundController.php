f774731471bcc6563d6db0ee3b09be1c34ba10a0
Ale Guis F <aleguisf@gmail.com>
2022-05-18 08:36:49 -0400

    bug fixes

--- app/Http/Controllers/RetirementFundController.php
+++ app/Http/Controllers/RetirementFundController.php
@@ -634,7 +634,7 @@ class RetirementFundController extends Controller
         }
         $applicant = RetFunBeneficiary::where('type', 'S')->where('retirement_fund_id', $retirement_fund->id)->first();
 
-        if (isset(RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first()->id)){
+        if (RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first()){
             $beneficiary_advisor = RetFunAdvisorBeneficiary::where('ret_fun_beneficiary_id', $applicant->id)->first();
             $advisor = RetFunAdvisor::find($beneficiary_advisor->ret_fun_advisor_id);
         }
