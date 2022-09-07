ac72a4394b7735e9941215b8ab4f51cf0acaebaf
MaxLyonel <leorexx100@gmail.com>
2022-09-07 19:15:43 -0400

    refactorización del middleware y migración de tabla affiliate tokens

--- app/Http/Middleware/ApiAuth.php
+++ app/Http/Middleware/ApiAuth.php
@@ -22,10 +22,15 @@ class ApiAuth
         if ($request->bearerToken()) {
             $device = AffiliateToken::whereApiToken($request->bearerToken())->first();
             if ($device) {
-                if (Hash::check($device->device_id, $device->api_token)) {
-                    $request->merge(['affiliate' => $device->affiliate]);
-                    return $next($request);
-                }
+                // if (Hash::check($device->device_id, $device->api_token)) {
+                //     logger("entra if");
+                //     $request->merge(['affiliate' => $device->affiliate]);
+                //     return $next($request);
+                // }
+                logger("entra aca");
+                $request->merge(['affiliate' => $device->affiliate]);
+                return $next($request); 
+               
             }
         } else {
             return response()->json([
