c71ded3d063f018c8df62c6c991b1e9d697a7659
gongor7 <crisgongor7@gmail.com>
2022-09-28 17:30:03 -0400

    Cambio a mayusculas en lectura del nombre de las imagenes

--- app/Http/Controllers/AffiliateController.php
+++ app/Http/Controllers/AffiliateController.php
@@ -381,23 +381,23 @@ class AffiliateController extends Controller
         $fotoIzquierdaViudedad="";
         $fotoDerechaViudedad="";
         $path = 'liveness/faces/'.$affiliate->id.'/deceased';
-        if (Storage::exists($path.'/Frente_viudedad.jpg'))
-            $fotoFrenteViudedad=base64_encode(Storage::get($path.'/Frente_viudedad.jpg'));
-        if (Storage::exists($path.'/Izquierda_viudedad.jpg'))
-            $fotoIzquierdaViudedad=base64_encode(Storage::get($path.'/Izquierda_viudedad.jpg'));
-        if (Storage::exists($path.'/Derecha_viudedad.jpg'))
-            $fotoDerechaViudedad=base64_encode(Storage::get($path.'/Derecha_viudedad.jpg'));
+        if (Storage::exists($path.'/Frente_Viudedad.jpg'))
+            $fotoFrenteViudedad=base64_encode(Storage::get($path.'/Frente_Viudedad.jpg'));
+        if (Storage::exists($path.'/Izquierda_Viudedad.jpg'))
+            $fotoIzquierdaViudedad=base64_encode(Storage::get($path.'/Izquierda_Viudedad.jpg'));
+        if (Storage::exists($path.'/Derecha_Viudedad.jpg'))
+            $fotoDerechaViudedad=base64_encode(Storage::get($path.'/Derecha_Viudedad.jpg'));
 
         $fotoFrenteVejez="";
         $fotoIzquierdaVejez="";
         $fotoDerechaVejez="";
         $path = 'liveness/faces/'.$affiliate->id.'/deceased';
-        if (Storage::exists($path.'/Frente_vejez.jpg'))
-            $fotoFrenteVejez=base64_encode(Storage::get($path.'/Frente_vejez.jpg'));
-        if (Storage::exists($path.'/Izquierda_vejez.jpg'))
-            $fotoIzquierdaVejez=base64_encode(Storage::get($path.'/Izquierda_vejez.jpg'));
-        if (Storage::exists($path.'/Derecha_vejez.jpg'))
-            $fotoDerechaVejez=base64_encode(Storage::get($path.'/Derecha_vejez.jpg'));
+        if (Storage::exists($path.'/Frente_Vejez.jpg'))
+            $fotoFrenteVejez=base64_encode(Storage::get($path.'/Frente_Vejez.jpg'));
+        if (Storage::exists($path.'/Izquierda_Vejez.jpg'))
+            $fotoIzquierdaVejez=base64_encode(Storage::get($path.'/Izquierda_Vejez.jpg'));
+        if (Storage::exists($path.'/Derecha_Vejez.jpg'))
+            $fotoDerechaVejez=base64_encode(Storage::get($path.'/Derecha_Vejez.jpg'));
 
         if(AffiliateToken::where('affiliate_id','=',$affiliate->id)->first())
             $affiliateDevice = AffiliateToken::where('affiliate_id','=',$affiliate->id)->first()->affiliate_device? AffiliateToken::where('affiliate_id','=',$affiliate->id)->first()->affiliate_device:NULL;
