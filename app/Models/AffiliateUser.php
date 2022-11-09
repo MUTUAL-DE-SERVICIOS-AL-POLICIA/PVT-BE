858e5b6422d36372abc7b0ad76e746baa650ce2a
gongor7 <crisgongor7@gmail.com>
2022-11-09 12:05:38 -0400

    Invalidacion de credenciales en oficina virtual cuando se registra fallecido al afiliado

---
+++ app/Models/AffiliateUser.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Muserpol;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AffiliateUser extends Model
+{
+    // use HasFactory;
+
+    public $timestamps = true;
+    protected $fillable = [
+        'affiliate_token_id',
+        'username',
+        'password',
+        'access_status',
+        'password_update_code'
+    ];
+    protected $primaryKey = 'affiliate_token_id';
+    public $incrementing = false;
+
+    public function affiliate_token()
+    {
+        return $this->belongsTo(AffiliateToken::class);
+    }
+}
