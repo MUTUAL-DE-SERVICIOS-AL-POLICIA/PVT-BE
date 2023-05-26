798f90c7004c8ca20f3751d8b159dd636d8c2e05
Noemi Ancari <noemiancari.na@gmail.com>
2023-05-26 10:33:56 -0400

    cambio en el reporte de afiliados con docs no escaneados

--- app/Exports/AffiliateNoScanner.php
+++ app/Exports/AffiliateNoScanner.php
@@ -11,9 +11,11 @@ class AffiliateNoScanner  implements FromCollection, WithHeadings, ShouldAutoSiz
     public function collection()
     {
         $data = null;
-        $query = "SELECT distinct a.id, concat(a.first_name, ' ', a.second_name, ' ', a.last_name, ' ', a.mothers_last_name) as fullname, a.identity_card
+        $query = "SELECT distinct a.id, a.identity_card, concat(a.first_name, ' ', a.second_name, ' ', a.last_name, ' ', a.mothers_last_name) as fullname, d.name as degree,c.name as category
         from affiliates a 
-        inner join economic_complements ec on a.id = ec.affiliate_id";
+        inner join economic_complements ec on a.id = ec.affiliate_id--9.513
+        left join degrees d on d.id = a.degree_id 
+        left join categories c on c.id = a.category_id";
         $data = DB::select($query);
         $data = collect($data);
 
@@ -33,8 +35,10 @@ class AffiliateNoScanner  implements FromCollection, WithHeadings, ShouldAutoSiz
         
         $default = [
             'NUP ',
-            'Nombre Completo',
             'CI',
+            'Nombre Completo afiliado',
+            'Grado',
+            'Categoría',
             '¿Documentos escaneados?'
         ];
         return array_merge($default, $new_columns);
