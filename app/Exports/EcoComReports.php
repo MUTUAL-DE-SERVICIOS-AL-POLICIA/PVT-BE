0e5d64c51add8aed2b371a67d7471f13eb7a4a5f
adrian <adrirha@gmail.com>
2024-07-02 10:13:42 -0400

    Se añadio la columna Type_rent a Todos los Trámites

--- app/Exports/EcoComReports.php
+++ app/Exports/EcoComReports.php
@@ -43,6 +43,7 @@ class EcoComReports implements FromCollection, WithHeadings, ShouldAutoSize
                     END,
                     ''
                 )) AS mensaje,
+                eco_com_updated_pensions.rent_type as AM_tipo_renta,
                 eco_com_updated_pensions.aps_total_fsa as AM_fraccion_saldo_acumulada_APS,
                 eco_com_updated_pensions.aps_total_cc as AM_fraccion_compensacion_cotizaciones_APS,
                 eco_com_updated_pensions.aps_total_fs as AM_fraccion_solidaria_vejez_APS,
@@ -108,6 +109,7 @@ class EcoComReports implements FromCollection, WithHeadings, ShouldAutoSize
                     "affiliate_devices.verified",
                     "affiliate_tokens.api_token",
                     "affiliate_tokens.firebase_token",
+                    "eco_com_updated_pensions.rent_type",
                     "eco_com_updated_pensions.aps_total_fsa",
                     "eco_com_updated_pensions.aps_total_cc",
                     "eco_com_updated_pensions.aps_total_fs",
@@ -250,6 +252,7 @@ class EcoComReports implements FromCollection, WithHeadings, ShouldAutoSize
                     "Contraste C.I",
                     "Notificación",
                     "Validado por",
+                    "AM_tipo_renta",
                     "AM_fraccion_saldo_acumulada_APS",
                     "AM_fraccion_compensacion_cotizaciones_APS",
                     "AM_fraccion_solidaria_vejez_APS",
