ffedd696b3c1c12a3ad740cac2e5e00f048a668c
MaxLyonel <leorexx100@gmail.com>
2024-08-12 17:49:10 -0400

    Agregando descuento por retención de juzgado al área de calificación de QAM

--- resources/views/quota_aid/beneficiaries_list.blade.php
+++ resources/views/quota_aid/beneficiaries_list.blade.php
@@ -56,8 +56,13 @@
 </quota-aid-beneficiaries-show>
 <ret-fun-beneficiary-testimony-list :beneficiaries="{{ $beneficiaries }}" :doc-id="{{ $quota_aid->id }}" type="quotaAid">
 </ret-fun-beneficiary-testimony-list>
-@if(Auth::user()->roleId == 38)
-<quota-aid-judicial-retention
-:quota-aid-id="{{ $quota_aid->id }}"
-></quota-aid-judicial-retention>
+
+@php
+    $role = Auth::user()->roles->firstWhere('id', 38);
+@endphp
+
+@if($role)
+    <quota-aid-judicial-retention
+    :quota-aid-id="{{ $quota_aid->id }}"
+    ></quota-aid-judicial-retention>
 @endif
