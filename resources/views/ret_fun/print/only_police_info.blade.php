781bf457b345a81dbde84aede10613c5a6d899a5
gladys850 <gpq850@gmail.com>
2025-01-28 11:33:22 -0400

    Incluir el campo NUP en todos los formularios utilizados para FR y pie en recepción, closes #2049

--- resources/views/ret_fun/print/only_police_info.blade.php
+++ resources/views/ret_fun/print/only_police_info.blade.php
@@ -1,6 +1,9 @@
 <table class="table-info w-100">
     <thead class="bg-grey-darker">
         <tr class="font-medium text-white text-sm">
+            <td class="px-15 text-center text-sm uppercase">
+                NUP
+            </td>
         @if($procedure_modality->procedure_type->id != 21)
             <td class="px-15 text-center text-sm uppercase">
                 fecha de ingreso
@@ -22,6 +25,7 @@
     </thead>
     <tbody>
         <tr class="text-sm">
+            <td class="text-center uppercase font-bold px-5 py-3">{{ $affiliate->id ?? null }}</td>
         @if($procedure_modality->procedure_type->id != 21)
             <td class="text-center uppercase font-bold px-5 py-3">{{ Util::formatMonthYear($affiliate->date_entry) }}</td>
             <td class="text-center uppercase font-bold px-5 py-3">{{ Util::formatMonthYear($affiliate->date_derelict) }}</td>
