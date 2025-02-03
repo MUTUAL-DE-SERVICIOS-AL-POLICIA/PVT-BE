2f357c23179ec61711f77cb24817eb7728fe2512
gladys850 <gpq850@gmail.com>
2025-02-03 17:46:34 -0400

    Adicion de la seccion retenciones judicales

--- routes/web.php
+++ routes/web.php
@@ -132,6 +132,10 @@ Route::group(['middleware' => ['auth']], function () {
     Route::post('ret_fun/{retirement_fund}/save_certification_note', 'RetirementFundController@saveCertificationNote')->name('save_certification_note');
     Route::post('procedure/print/send', 'InboxController@printSend')->name('inbox_send');
     Route::post('procedure/print/send_eco_com', 'InboxController@printSendEcoCom')->name('inbox_send_eco_com');
+    Route::post('ret_fun/{ret_fun}/save_judicial_retention', 'RetirementFundController@createJudicialRetention');
+    Route::get('ret_fun/{ret_fun}/obtain_judicial_retention', 'RetirementFundController@obtainJudicialRetention');
+    Route::patch('ret_fun/{ret_fun}/modify_judicial_retention', 'RetirementFundController@modifyJudicialRetention');
+    Route::delete('ret_fun/{ret_fun}/cancel_judicial_retention', 'RetirementFundController@cancelJudicialRetention');
 
     //Quota Aid Certification
     Route::get('quota_aid/{affiliate}/print/quota_aid_commitment_letter', 'QuotaAidCertificationController@printQuotaAidCommitmentLetter')->name('print_quota_aid_commitment_letter');
