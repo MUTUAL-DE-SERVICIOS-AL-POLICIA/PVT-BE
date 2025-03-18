bca4fb9c16ed1e4fc9c1b31b61ed7536a6b3a38c
Adrian-Herrera-96 <adrirha@gmail.com>
2025-03-18 10:29:48 -0400

    Correción de wf_sequences de seeder

--- database/seeds/UpdateRetirementFundRegulations2024.php
+++ database/seeds/UpdateRetirementFundRegulations2024.php
@@ -38,15 +38,13 @@ class UpdateRetirementFundRegulations2024 extends Seeder
         ]);
 
         DB::table('wf_states')->insert([
-            ['module_id' => 3, 'role_id' => 103, 'name' => 'Área de Liquidación Fondo de Retiro', 'first_shortened' => 'Liquidación', 'sequence_number' => 7, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+            ['module_id' => 3, 'role_id' => 104, 'name' => 'Área de Liquidación Fondo de Retiro', 'first_shortened' => 'Liquidación', 'sequence_number' => 7, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
         ]);
 
-        DB::table('wf_sequences')->insert([
-            ['workflow_id' => 4, 'wf_state_current_id' => 24, 'wf_state_next_id' => 62, 'action' => 'Aprobar', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
-            ['workflow_id' => 4, 'wf_state_current_id' => 62, 'wf_state_next_id' => 47, 'action' => 'Aprobar', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
-        ]);        
+        DB::table('wf_sequences')->where('wf_state_current_id', 24)->update(['wf_state_next_id' => 63, 'updated_at' => Carbon::now()]);
+        DB::table('wf_sequences')->where('wf_state_next_id', 47)->update(['wf_state_current_id' => 63, 'updated_at' => Carbon::now()]);       
 
         // Borrado logico de Resolucion
-        DB::table('wf_states')->where('name', 'Área de Resolución Fondo de Retiro')->update(['deleted_at' => Carbon::now()]);
+        // DB::table('wf_states')->where('name', 'Área de Resolución Fondo de Retiro')->update(['deleted_at' => Carbon::now()]);
     }
 }
