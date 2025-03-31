70ec14ce79c9c58bb2287ac7f92e1294dac7e21c
Adrian-Herrera-96 <adrirha@gmail.com>
2025-03-31 14:49:19 -0400

    Se refactorizo un seeder

--- database/seeds/UpdateRetirementFundRegulations2024.php
+++ database/seeds/UpdateRetirementFundRegulations2024.php
@@ -3,6 +3,10 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use Muserpol\Models\Role;
+use Muserpol\Helpers\ID;
+use Muserpol\Models\Workflow\WorkflowSequence;
+use Muserpol\Models\Workflow\WorkflowState;
 
 class UpdateRetirementFundRegulations2024 extends Seeder
 {
@@ -33,16 +37,23 @@ class UpdateRetirementFundRegulations2024 extends Seeder
         DB::statement('ALTER TABLE discount_type_retirement_fund DROP CONSTRAINT discount_type_retirement_fund_discount_type_id_retirement_fund_');
 
         // Insertar nueva modalidad de liquidación
-        DB::table('roles')->insert([
-            ['module_id' => 3, 'display_name' => 'Área de Liquidación', 'action' => 'Aprobado', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now(), 'name' => 'FR-area-de-liquidacion']
+        $role = Role::create([
+            'module_id'    => ID::module()->ret_fun,
+            'display_name' => 'Área de Liquidación',
+            'action'       => 'Aprobado',
+            'name'         => 'FR-area-de-liquidacion',
         ]);
 
-        DB::table('wf_states')->insert([
-            ['module_id' => 3, 'role_id' => 104, 'name' => 'Área de Liquidación Fondo de Retiro', 'first_shortened' => 'Liquidación', 'sequence_number' => 7, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()]
+        $wf_state = WorkflowState::create([
+            'module_id'    => ID::module()->ret_fun,
+            'role_id' => $role->id,
+            'name' => 'Área de Liquidación Fondo de Retiro',
+            'first_shortened' => 'Liquidación',
+            'sequence_number' => 7,
         ]);
 
-        DB::table('wf_sequences')->where('wf_state_current_id', 24)->update(['wf_state_next_id' => 63, 'updated_at' => Carbon::now()]);
-        DB::table('wf_sequences')->where('wf_state_next_id', 47)->update(['wf_state_current_id' => 63, 'updated_at' => Carbon::now()]);       
+        DB::table('wf_sequences')->where('wf_state_current_id', 24)->update(['wf_state_next_id' => $wf_state->id, 'updated_at' => Carbon::now()]);
+        DB::table('wf_sequences')->where('wf_state_next_id', 47)->update(['wf_state_current_id' => $wf_state->id, 'updated_at' => Carbon::now()]);       
 
         // Borrado logico de Resolucion
         // DB::table('wf_states')->where('name', 'Área de Resolución Fondo de Retiro')->update(['deleted_at' => Carbon::now()]);
