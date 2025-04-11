c98347b36a98385b9aeb7a790443d6b57f47c943
gladys850 <gpq850@gmail.com>
2025-04-11 08:59:04 -0400

    adición a parametros de calificación y bu8g fixed obtenciónde grados

--- app/Http/Controllers/BaseWageController.php
+++ app/Http/Controllers/BaseWageController.php
@@ -26,10 +26,10 @@ class BaseWageController extends Controller
     }
     public function FirstLevelData()
     {
-        $select = DB::raw("base_wages.month_year as month_year, c1.amount as c1, c2.amount as c2, c3.amount as c3, c4.amount as c4, c5.amount as c5, c6.amount as c6, c7.amount as c7, c8.amount as c8, c9.amount as c9, c10.amount as c10, c11.amount as c11, c12.amount as c12");
+        $select = DB::raw("base_wages.month_year as month_year, c1.amount as c1, c3.amount as c3, c4.amount as c4, c5.amount as c5, c6.amount as c6, c7.amount as c7, c8.amount as c8, c9.amount as c9, c10.amount as c10, c11.amount as c11, c12.amount as c12");
         $base_wages = DB::table('base_wages')->select($select)
             ->leftJoin('base_wages as c1', 'c1.month_year', '=', 'base_wages.month_year')
-            ->leftJoin('base_wages as c2', 'c2.month_year', '=', 'base_wages.month_year')
+            //->leftJoin('base_wages as c2', 'c2.month_year', '=', 'base_wages.month_year')
             ->leftJoin('base_wages as c3', 'c3.month_year', '=', 'base_wages.month_year')
             ->leftJoin('base_wages as c4', 'c4.month_year', '=', 'base_wages.month_year')
             ->leftJoin('base_wages as c5', 'c5.month_year', '=', 'base_wages.month_year')
@@ -41,7 +41,7 @@ class BaseWageController extends Controller
             ->leftJoin('base_wages as c11', 'c11.month_year', '=', 'base_wages.month_year')
             ->leftJoin('base_wages as c12', 'c12.month_year', '=', 'base_wages.month_year')
             ->where('c1.degree_id', '=', '1')
-            ->where('c2.degree_id', '=', '2')
+            //->where('c2.degree_id', '=', '2')
             ->where('c3.degree_id', '=', '3')
             ->where('c4.degree_id', '=', '4')
             ->where('c5.degree_id', '=', '5')
@@ -52,7 +52,7 @@ class BaseWageController extends Controller
             ->where('c10.degree_id', '=', '10')
             ->where('c11.degree_id', '=', '11')
             ->where('c12.degree_id', '=', '12')
-            ->groupBy('base_wages.month_year', 'c1.amount', 'c2.amount', 'c3.amount', 'c4.amount', 'c5.amount', 'c5.amount', 'c6.amount', 'c7.amount', 'c8.amount', 'c9.amount', 'c10.amount', 'c11.amount', 'c12.amount');
+            ->groupBy('base_wages.month_year', 'c1.amount', 'c3.amount', 'c4.amount', 'c5.amount', 'c5.amount', 'c6.amount', 'c7.amount', 'c8.amount', 'c9.amount', 'c10.amount', 'c11.amount', 'c12.amount');
         return Datatables::of($base_wages)
             ->editColumn('month_year', function ($base_wage) {
                 return Carbon::parse($base_wage->month_year)->year;
@@ -60,9 +60,9 @@ class BaseWageController extends Controller
             ->editColumn('c1', function ($base_wage) {
                 return Util::formatMoney($base_wage->c1);
             })
-            ->editColumn('c2', function ($base_wage) {
-                return Util::formatMoney($base_wage->c2);
-            })
+            // ->editColumn('c2', function ($base_wage) {
+            //     return Util::formatMoney($base_wage->c2);
+            // })
             ->editColumn('c3', function ($base_wage) {
                 return Util::formatMoney($base_wage->c3);
             })
@@ -326,7 +326,7 @@ class BaseWageController extends Controller
         if (!$validator->fails()) {
             switch($level){
                 case 1:
-                    $degrees = Degree::where('hierarchy_id',1)->orWhere('hierarchy_id',2)->orderBy('id')->get();
+                    $degrees = Degree::where('hierarchy_id',1)->where('is_active', true)->orWhere('hierarchy_id',2)->orderBy('id')->get();
                     $c = 0;
                     if($degrees->count() == count($base_wages)){
                         foreach($base_wages as $base_wage)
@@ -343,7 +343,7 @@ class BaseWageController extends Controller
                     }
                     break;
                 case 2:
-                    $degrees = Degree::where('hierarchy_id',3)->orderBy('id')->get();
+                    $degrees = Degree::where('hierarchy_id',3)->where('is_active', true)->orderBy('id')->get();
                     $c = 0;
                     if($degrees->count() == count($base_wages)){
                         foreach($base_wages as $base_wage)
@@ -360,7 +360,7 @@ class BaseWageController extends Controller
                     }
                     break;
                 case 3:
-                    $degrees = Degree::where('hierarchy_id',4)->orderBy('id')->get();
+                    $degrees = Degree::where('hierarchy_id',4)->where('is_active', true)->orderBy('id')->get();
                     $c = 0;
                     if($degrees->count() == count($base_wages)){
                         foreach($base_wages as $base_wage)
@@ -377,7 +377,7 @@ class BaseWageController extends Controller
                     }
                     break;
                 case 4:
-                    $degrees = Degree::where('hierarchy_id',5)->orderBy('id')->get();
+                    $degrees = Degree::where('hierarchy_id',5)->where('is_active', true)->orderBy('id')->get();
                     $c = 0;
                     if($degrees->count() == count($base_wages)){
                         foreach($base_wages as $base_wage)
