1d27ece61f984f965a28b301568a310bcf6efd1b
gongor7 <crisgongor7@gmail.com>
2025-08-14 14:51:47 -0400

    Adición de validación para no recibir paramatros en la opción de gestión documental

--- resources/assets/js/components/eco_com/SelectReport.vue
+++ resources/assets/js/components/eco_com/SelectReport.vue
@@ -4,7 +4,7 @@
     <select v-model="form.reportTypeId" :disabled="loadingButton">
       <option v-for="r in reportsType" :value="r.id" :key="r.id">{{r.name}}</option>
     </select>
-    <div v-if="form.reportTypeId != 9 && form.reportTypeId != 21 && form.reportTypeId != 22 && form.reportTypeId != 29 && form.reportTypeId != 30">
+    <div v-if="form.reportTypeId != 9 && form.reportTypeId != 21 && form.reportTypeId != 22 && form.reportTypeId != 29 && form.reportTypeId != 30 && form.reportTypeId != 31">
       <label>Gestion</label>
       <select v-model="form.ecoComProcedureId" :disabled="loadingButton" @change="cargarplanilla()">
         <option v-for="r in ecoComProcedures" :value="r.id" :key="r.id">{{r.full_name}}</option>
