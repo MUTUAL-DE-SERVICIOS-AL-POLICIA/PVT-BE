9a7dc3ca2f290d4514d4ffeec3399555e58c9e86
napoleonbaley2011 <leonellima56@gmail.com>
2025-10-06 10:23:50 -0400

    Solitara actualizacion de la app a la nueva version

--- app/Http/Controllers/API/VersionController.php
+++ app/Http/Controllers/API/VersionController.php
@@ -22,7 +22,7 @@ class VersionController extends Controller
         switch($store) {
             case 'playstore':
                 $url_store = 'https://play.google.com/store/apps/details?id=com.muserpol.pvt';
-                return ($version == "3.0.8" || $version == "3.0.9")? $this->get_version(true, $url_store) : $this->get_version(false, $url_store);
+                return ($version == "4.0.1")? $this->get_version(true, $url_store) : $this->get_version(false, $url_store);
             case 'appstore':
                 $url_store = 'https://apps.apple.com/app/id284815942';
                 return $version == "3.0.9" ? $this->get_version(true, $url_store) : $this->get_version(false, $url_store);
